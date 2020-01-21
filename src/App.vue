c79073d22639d10e105f8dccb62af619712cc7c7
Tai Sakuma <tai.sakuma@gmail.com>
2020-01-21 13:49:00 -0500

    replace HelloWorld with route-view in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
   <v-app>
     <v-content>
-      <HelloWorld />
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
     //
   })
