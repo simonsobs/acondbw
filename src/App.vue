c9f9e52dd8818595c9ad63ed02a5b3560ba1db41
Tai Sakuma <tai.sakuma@gmail.com>
2020-01-24 15:09:05 -0500

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list>
+        <v-list-item link router :to="'/'">
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link router :to="'/about'">
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link router :to="'/maps'">
+          <v-list-item-content>
+            <v-list-item-title>Maps</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app dense clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+    </v-app-bar>
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -10,7 +32,7 @@
 export default {
   name: "App",
   data: () => ({
-    //
-  })
+    drawer: null
+  }),
 };
 </script>
