a98fbb4d0fbe8a16ceb26381bba8e28ecd86c42b
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-06 16:07:22 -0500

    replace "an" with "the" in the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fill-height fluid>
     <v-row align="center" justify="center">
-      <h1>This is an home page</h1>
+      <h1>This is the home page</h1>
     </v-row>
   </v-container>
 </template>
