46d5f5c67dcc1641d54fa9cd38e50863853bdeb5
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-13 16:14:05 -0500

    allow no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     'vue/no-unused-components': 'off',
+    'no-unused-vars': 'off',
     'no-unused-labels': 'off',
   },
   parserOptions: {
