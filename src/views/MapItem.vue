e39d8d4e5ad320e695f64cf90009a3b0813e8674
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-13 15:01:20 -0500

    add the button to back to maps in MapItem

--- src/views/MapItem.vue
+++ src/views/MapItem.vue
@@ -2,7 +2,16 @@
   <div class="mapitem">
     <v-container>
       <h2>Map</h2>
-      <p v-text="$route.params.name"></p>
+      <div class="d-flex justify-start my-2">
+        <v-tooltip bottom open-delay="800">
+          <template v-slot:activator="{ on }">
+            <v-btn text icon exact to="/maps" v-on="on">
+              <v-icon>mdi-arrow-left</v-icon>
+            </v-btn>
+          </template>
+          <span>Back to Maps</span>
+        </v-tooltip>
+      </div>
       <MapItemCard
         :map="item"
         :collapsible="false"
