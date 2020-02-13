ec1e9db3a97c93541e5c59bf8c5d64d2d8fd8c19
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-13 15:16:46 -0500

    clean code in the view Beams

--- src/views/Beams.vue
+++ src/views/Beams.vue
@@ -5,15 +5,15 @@
       <div class="d-flex justify-end ma-2">
         <v-tooltip bottom open-delay="800">
           <template v-slot:activator="{ on }">
-            <v-btn icon @click="showsAny = !showsAny" v-on="on">
-              <v-icon>{{ showsAny ? 'mdi-unfold-less-horizontal' : 'mdi-unfold-more-horizontal' }}</v-icon>
+            <v-btn icon @click="areAllCardsCollapsed = !areAllCardsCollapsed" v-on="on">
+              <v-icon>{{ areAllCardsCollapsed ? 'mdi-unfold-more-horizontal' : 'mdi-unfold-less-horizontal' }}</v-icon>
             </v-btn>
           </template>
-          <span>{{ showsAny ? 'Collapse all' : 'Expand all' }}</span>
+          <span>{{ areAllCardsCollapsed ? 'Expand all' : 'Collapse all' }}</span>
         </v-tooltip>
       </div>
       <v-card outlined hover v-for="edge in edges" :key="edge.node.id" style="cursor: default;">
-        <div @click="shows[edge.node.id] = !shows[edge.node.id]">
+        <div @click="isCardCollapsed[edge.node.id] = !isCardCollapsed[edge.node.id]">
           <v-layout row wrap class="ma-0 px-3">
             <v-flex xs12 md3>
               <div class="caption grey--text">Name</div>
@@ -24,13 +24,20 @@
               <div v-text="edge.node.path"></div>
             </v-flex>
             <v-flex xs1 md1 align-self-end>
-              <v-btn icon>
-                <v-icon>{{ shows[edge.node.id] ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-              </v-btn>
+              <v-layout justify-end>
+                <v-tooltip bottom open-delay="800">
+                  <template v-slot:activator="{ on }">
+                    <v-btn icon v-on="on">
+                      <v-icon>{{ isCardCollapsed[edge.node.id] ? 'mdi-chevron-down' : 'mdi-chevron-up' }}</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>{{ isCardCollapsed[edge.node.id] ? 'Expand' : 'Collapse' }}</span>
+                </v-tooltip>
+              </v-layout>
             </v-flex>
           </v-layout>
           <v-expand-transition>
-            <v-layout row wrap class="mx-0 my-3 px-3" v-show="shows[edge.node.id]">
+            <v-layout row wrap class="mx-0 my-3 px-3" v-show="!isCardCollapsed[edge.node.id]">
               <v-flex xs12 md4 offset-md-3>
                 <div class="caption grey--text">Map</div>
                 <div v-if="edge.node.map" v-text="edge.node.map.name"></div>
@@ -54,19 +61,20 @@ export default {
   name: "beams",
   data() {
     return {
-      headers: [],
       edges: [],
-      shows: {}
+      isCardCollapsed: {}
     };
   },
   computed: {
-    showsAny: {
+    areAllCardsCollapsed: {
       get: function() {
-        return Object.keys(this.shows).some(i => this.shows[i]);
+        return Object.keys(this.isCardCollapsed).every(
+          i => this.isCardCollapsed[i]
+        );
       },
       set: function(v) {
-        for (const k in this.shows) {
-          this.shows[k] = v;
+        for (const k in this.isCardCollapsed) {
+          this.isCardCollapsed[k] = v;
         }
       }
     }
@@ -76,8 +84,8 @@ export default {
   },
   methods: {
     loadData() {
-       const url = process.env.VUE_APP_ACONDBS_URL;
-       const query = `
+      const url = process.env.VUE_APP_ACONDBS_URL;
+      const query = `
         { allBeams(sort: NAME_DESC) {
           edges {
             node {
@@ -102,8 +110,8 @@ export default {
         }
       }).then(response => {
         this.edges = response.data.data.allBeams.edges;
-        this.shows = this.edges.reduce(
-          (obj, x) => ({ ...obj, [x.node.id]: false }),
+        this.isCardCollapsed = this.edges.reduce(
+          (obj, x) => ({ ...obj, [x.node.id]: true }),
           {}
         );
       });
