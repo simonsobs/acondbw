a0d9d0e813b50ddda69995755b5f83dcbd900a26
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-13 14:24:29 -0500

    use the component MapItemCard in the view MapItem

--- src/views/MapItem.vue
+++ src/views/MapItem.vue
@@ -3,45 +3,10 @@
     <v-container>
       <h2>Map</h2>
       <p v-text="$route.params.name"></p>
-      <v-card outlined hover style="cursor: default;">
-        <v-layout row wrap class="ma-0 px-3">
-          <v-flex xs12 md4>
-            <div class="caption grey--text">Name</div>
-            <div class="font-weight-medium primary--text" v-text="item.name"></div>
-          </v-flex>
-          <v-flex xs6 md4>
-            <div class="caption grey--text">Date posted</div>
-            <div v-text="item.datePosted"></div>
-          </v-flex>
-          <v-flex xs5 md3>
-            <div class="caption grey--text">Mapper</div>
-            <div v-text="item.mapper"></div>
-          </v-flex>
-        </v-layout>
-        <v-layout row wrap class="mx-0 my-3 px-3">
-          <v-flex xs12 md8 offset-md-4>
-            <div class="caption grey--text">Note</div>
-            <div>
-              <ul v-if="item.note">
-                <li
-                  v-for="(line, index) in item.note.split('\n')"
-                  :key="index"
-                >{{ line.replace(/^- */, "") }}</li>
-              </ul>
-            </div>
-          </v-flex>
-          <v-flex xs12 md8 offset-md-4>
-            <div class="caption grey--text">Paths</div>
-            <ul v-if="item.mapFilePaths">
-              <li
-                v-for="(edgep, index) in item.mapFilePaths.edges"
-                :key="index"
-                v-text="edgep.node.path"
-              ></li>
-            </ul>
-          </v-flex>
-        </v-layout>
-      </v-card>
+      <MapItemCard
+        :map="item"
+        collapsible="false"
+      ></MapItemCard>
     </v-container>
   </div>
 </template>
@@ -49,8 +14,13 @@
 <script>
 import axios from "axios";
 
+import MapItemCard from "@/components/MapItemCard";
+
 export default {
   name: "mapItem",
+  components: {
+    MapItemCard
+  },
   data() {
     return {
       item: {}
