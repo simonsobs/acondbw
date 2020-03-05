7463cd99c57b5c74f75c3127af074d7bd72bdf8b
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-05 17:21:06 -0500

    update test for Maps.vue

--- tests/unit/views/Maps.spec.js
+++ tests/unit/views/Maps.spec.js
@@ -31,87 +31,18 @@ describe("Maps.vue", () => {
       node: {
         id: "TWFwOjEwMTM=",
         name: "lat20200201",
-        datePosted: "2020-02-01",
-        mapper: "pwg-pmn",
-        note:
-          "- This is a dummy test with a lat map                         \n- A beam depends on this map",
-        mapFilePaths: {
-          edges: [
-            {
-              node: {
-                path: "nersc:/go/to/my/maps_v3",
-                note: "lat only"
-              }
-            }
-          ]
-        },
-        beams: {
-          edges: [
-            {
-              node: {
-                name: "20200207"
-              }
-            }
-          ]
-        }
       }
     },
     {
       node: {
         id: "TWFwOjEwMTI=",
         name: "lat20200120",
-        datePosted: "2020-01-20",
-        mapper: "pwg-pmn",
-        note:
-          "- This is a dummy test with a lat map                                             \n- A beam depends on this map",
-        mapFilePaths: {
-          edges: [
-            {
-              node: {
-                path: "nersc:/go/to/my/maps_v2",
-                note: "lat only"
-              }
-            },
-            {
-              node: {
-                path: "abcde:/path/to/the/maps_v2",
-                note: "lat only"
-              }
-            }
-          ]
-        },
-        beams: {
-          edges: [
-            {
-              node: {
-                name: "20200123"
-              }
-            }
-          ]
-        }
       }
     },
     {
       node: {
         id: "TWFwOjEwMDE=",
         name: "lat20190213",
-        datePosted: "2019-02-13",
-        mapper: "pwg-pmn",
-        note:
-          "- This is a dummy test with a lat map\n- This should not depend on any beam",
-        mapFilePaths: {
-          edges: [
-            {
-              node: {
-                path: "nersc:/go/to/my/maps",
-                note: ""
-              }
-            }
-          ]
-        },
-        beams: {
-          edges: []
-        }
       }
     }
   ];
