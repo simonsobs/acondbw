3c0ab29dec6e5489a993b3613e0bac243630a3fd
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-09 09:48:32 -0400

    update jest.config.js, enabling coverage

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,11 @@ module.exports = {
   transform: {
     "\\.(gql|graphql)$": "./node_modules/jest-transform-graphql"
     /* ... */
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["**/src/**", '**/*.{vue}', '!**/node_modules/**', '!<rootDir>/dist/**',
+                        '!<rootDir>/src/plugins/**', '!<rootDir>/tests/unit/**',
+                        '!**/**/MapItemWithApolloQuery.vue'
+                       ],
+  coverageReporters: ['lcov', 'text-summary']
 };
