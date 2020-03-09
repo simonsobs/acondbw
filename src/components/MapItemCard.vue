697634c8c7ea0d90647fc1bf880e638a4b2a352d
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-09 18:03:35 -0400

    replace v-layout, f-flex with v-row, v-col

--- src/components/MapItemCard.vue
+++ src/components/MapItemCard.vue
@@ -1,95 +1,89 @@
 <template>
   <div class="mapitemcard">
-  <div v-if="$apollo.queries.map.loading">loading...</div>
-  <div v-else-if="error">Error: cannot load data</div>
-  <v-card v-else-if="map" outlined hover style="max-width: 980px;" >
-    <div @click="$emit('expand')" style="cursor: default;">
-      <v-layout row wrap class="ma-0 px-3">
-        <v-flex xs12 md4>
-          <div class="caption grey--text">Name</div>
-          <div class="font-weight-medium primary--text">
-            <span @click.stop>
-              <router-link
-                :to="'/maps/item/' + map.name"
-                v-text="map.name"
-              ></router-link>
-            </span>
-          </div>
-        </v-flex>
-        <v-flex xs6 md4>
-          <div class="caption grey--text">Date posted</div>
-          <div v-text="map.datePosted"></div>
-        </v-flex>
-        <v-flex xs5 md3>
-          <div class="caption grey--text">Mapper</div>
-          <div v-text="map.mapper"></div>
-        </v-flex>
-        <v-flex xs1 md1 align-self-end>
-          <div v-if="collapsible">
-            <v-layout justify-end>
-              <v-tooltip bottom open-delay="800">
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    icon
-                    @click.stop="
+    <div v-if="$apollo.queries.map.loading">loading...</div>
+    <div v-else-if="error">Error: cannot load data</div>
+    <v-card v-else-if="map" outlined hover style="max-width: 980px;">
+      <div @click="$emit('expand')" style="cursor: default;">
+        <v-container fluid class="pa-0">
+          <v-row class="ma-0 px-0">
+            <v-col cols="12" md="4" class="pa-2">
+              <div class="caption grey--text">Name</div>
+              <div class="font-weight-medium primary--text">
+                <span @click.stop>
+                  <router-link :to="'/maps/item/' + map.name" v-text="map.name"></router-link>
+                </span>
+              </div>
+            </v-col>
+            <v-col cols="6" md="4" class="pa-2">
+              <div class="caption grey--text">Date posted</div>
+              <div v-text="map.datePosted"></div>
+            </v-col>
+            <v-col cols="5" md="3" class="pa-2">
+              <div class="caption grey--text">Mapper</div>
+              <div v-text="map.mapper"></div>
+            </v-col>
+            <v-col cols="1" md="1" align-self="end" class="pa-2">
+              <div v-if="collapsible">
+                <v-row align="start" justify="end" class="px-1">
+                  <v-tooltip bottom open-delay="800">
+                    <template v-slot:activator="{ on }">
+                      <v-btn
+                        icon
+                        @click.stop="
                       collapsed ? $emit('expand') : $emit('collapse')
                     "
-                    v-on="on"
-                  >
-                    <v-icon>{{
-                      collapsed ? "mdi-chevron-down" : "mdi-chevron-up"
-                    }}</v-icon>
-                  </v-btn>
-                </template>
-                <span>{{ collapsed ? "Expand" : "Collapse" }}</span>
-              </v-tooltip>
-            </v-layout>
-          </div>
-        </v-flex>
-      </v-layout>
-      <v-expand-transition>
-        <v-layout
-          row
-          wrap
-          class="mx-0 mb-3 px-3"
-          v-show="!(collapsible && collapsed)"
-        >
-          <v-flex xs12 md8 offset-md-4>
-            <div class="caption grey--text">Paths</div>
-            <ul v-if="map.mapFilePaths">
-              <li
-                v-for="(edgep, index) in map.mapFilePaths.edges"
-                :key="index"
-                v-text="edgep.node.path"
-              ></li>
-            </ul>
-          </v-flex>
-          <v-flex xs12 md8 offset-md-4>
-            <div class="caption grey--text">Note</div>
-            <div>
-              <ul v-if="map.note">
-                <li v-for="(line, index) in map.note.split('\n')" :key="index">
-                  {{ line.replace(/^- */, "") }}
-                </li>
-              </ul>
-            </div>
-          </v-flex>
-          <v-flex xs12 md8 offset-md-4>
-            <div class="caption grey--text">Beams</div>
-            <ul v-if="map.beams">
-              <li v-for="(edgep, index) in map.beams.edges" :key="index">
-                <router-link
-                  :to="'/beams/item/' + edgep.node.name"
-                  v-text="edgep.node.name"
-                ></router-link>
-              </li>
-            </ul>
-          </v-flex>
-        </v-layout>
-      </v-expand-transition>
-    </div>
-  </v-card>
-  <div v-else>Nothing to show here.</div>
+                        v-on="on"
+                      >
+                        <v-icon>
+                          {{
+                          collapsed ? "mdi-chevron-down" : "mdi-chevron-up"
+                          }}
+                        </v-icon>
+                      </v-btn>
+                    </template>
+                    <span>{{ collapsed ? "Expand" : "Collapse" }}</span>
+                  </v-tooltip>
+                </v-row>
+              </div>
+            </v-col>
+          </v-row>
+          <v-expand-transition>
+            <v-row class="mx-0 mb-3 px-0" v-show="!(collapsible && collapsed)">
+              <v-col cols="12" md="8" offset-md="4" class="pa-2">
+                <div class="caption grey--text">Paths</div>
+                <ul v-if="map.mapFilePaths">
+                  <li
+                    v-for="(edgep, index) in map.mapFilePaths.edges"
+                    :key="index"
+                    v-text="edgep.node.path"
+                  ></li>
+                </ul>
+              </v-col>
+              <v-col cols="12" md="8" offset-md="4" class="pa-2">
+                <div class="caption grey--text">Note</div>
+                <div>
+                  <ul v-if="map.note">
+                    <li
+                      v-for="(line, index) in map.note.split('\n')"
+                      :key="index"
+                    >{{ line.replace(/^- */, "") }}</li>
+                  </ul>
+                </div>
+              </v-col>
+              <v-col cols="12" md="8" offset-md="4" class="pa-2">
+                <div class="caption grey--text">Beams</div>
+                <ul v-if="map.beams">
+                  <li v-for="(edgep, index) in map.beams.edges" :key="index">
+                    <router-link :to="'/beams/item/' + edgep.node.name" v-text="edgep.node.name"></router-link>
+                  </li>
+                </ul>
+              </v-col>
+            </v-row>
+          </v-expand-transition>
+        </v-container>
+      </div>
+    </v-card>
+    <div v-else>Nothing to show here.</div>
   </div>
 </template>
 
@@ -115,7 +109,7 @@ export default {
       variables() {
         return {
           name: this.mapName
-        }
+        };
       },
       result(result) {
         this.error = null;
@@ -125,6 +119,5 @@ export default {
       }
     }
   }
-  
 };
 </script>
