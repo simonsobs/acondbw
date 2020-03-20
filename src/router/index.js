d03e2451a9d4d24341f055ad37064e6958252a8c
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-20 14:36:12 -0400

    clean code in router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -13,34 +13,29 @@ const routes = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "about" */ "@/views/About.vue")
   },
   {
     path: "/maps/item/:name",
     name: "MapItem",
     component: () =>
-      import(/* webpackChunkName: "mapItem" */ "../views/MapItem.vue")
+      import(/* webpackChunkName: "mapItem" */ "@/views/MapItem.vue")
   },
   {
     path: "/maps",
     name: "map",
-    component: () => import(/* webpackChunkName: "maps" */ "../views/Maps.vue")
+    component: () => import(/* webpackChunkName: "maps" */ "@/views/Maps.vue")
   },
   {
     path: "/beams/item/:name",
     name: "BeamItem",
     component: () =>
-      import(/* webpackChunkName: "beamItem" */ "../views/BeamItem.vue")
+      import(/* webpackChunkName: "beamItem" */ "@/views/BeamItem.vue")
   },
   {
     path: "/beams",
     name: "beam",
-    component: () =>
-      import(/* webpackChunkName: "beams" */ "../views/Beams.vue")
+    component: () => import(/* webpackChunkName: "beams" */ "@/views/Beams.vue")
   }
 ];
 
