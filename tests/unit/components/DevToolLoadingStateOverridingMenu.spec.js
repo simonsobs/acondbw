845f1624989e4c2f9e134668f85f2312cb115963
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-20 17:24:32 -0400

    add tests for DevToolLoadingStateOverridingMenu

---
+++ tests/unit/components/DevToolLoadingStateOverridingMenu.spec.js
@@ -0,0 +1,53 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Vuetify from "vuetify";
+import { mount, createLocalVue } from "@vue/test-utils";
+
+import DevToolLoadingStateOverridingMenu from "@/components/DevToolLoadingStateOverridingMenu";
+import State from "@/utils/LoadingState.js";
+
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+describe("DevToolLoadingStateOverridingMenu.vue", () => {
+  let localVue;
+  let vuetify;
+
+  beforeEach(function() {
+    localVue = createLocalVue();
+    vuetify = new Vuetify();
+  });
+
+  function createWrapper() {
+    let wrapper = mount(DevToolLoadingStateOverridingMenu, {
+      localVue,
+      vuetify
+    });
+
+    return wrapper;
+  }
+
+  it("emit a state", async () => {
+    const wrapper = createWrapper();
+    wrapper.setData({
+        state: State.LOADING
+      });
+    await Vue.nextTick();
+    expect(wrapper.emitted("state").length).toBe(1);
+    expect(wrapper.emitted("state")[0]).toEqual([State.LOADING]);
+    });
+
+  it("off - emit null", async () => {
+    const wrapper = createWrapper();
+    wrapper.setData({
+        state: State.LOADING
+      });
+    await Vue.nextTick();
+    wrapper.setData({
+        state: "off"
+      });
+    await Vue.nextTick();
+    expect(wrapper.emitted("state").length).toBe(2);
+    expect(wrapper.emitted("state")[1]).toEqual([null]);
+  });
+});
