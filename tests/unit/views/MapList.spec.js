e8491adc7db54b4399fb8747a841ace559310d00
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-20 18:38:41 -0400

    split the view Maps

---
+++ tests/unit/views/MapList.spec.js
@@ -0,0 +1,99 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Vuetify from "vuetify";
+import { mount, createLocalVue } from "@vue/test-utils";
+
+import MapList from "@/views/MapList.vue";
+
+import router from "@/router";
+
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+describe("MapList.vue", () => {
+  let localVue;
+
+  function createWrapper(loading = false) {
+    return mount(MapList, {
+      localVue,
+      router,
+      mocks: {
+        $apollo: {
+          queries: {
+            allMaps: {
+              loading: loading
+            }
+          }
+        }
+      },
+      stubs: {
+        MapItemCard: true,
+        MapAddForm: true
+      }
+    });
+  }
+
+  const allMaps = {
+    edges: [
+      {
+        node: {
+          id: "TWFwOjEwMTM=",
+          mapId: "1013",
+          name: "lat20200201"
+        }
+      },
+      {
+        node: {
+          id: "TWFwOjEwMTI=",
+          mapId: "1012",
+          name: "lat20200120"
+        }
+      },
+      {
+        node: {
+          id: "TWFwOjEwMDE=",
+          mapId: "1001",
+          name: "lat20190213"
+        }
+      }
+    ]
+  };
+
+  beforeEach(function() {
+    localVue = createLocalVue();
+  });
+
+  it("match snapshot", async () => {
+    const wrapper = createWrapper();
+    wrapper.setData({
+      allMaps: allMaps
+    });
+    await Vue.nextTick();
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("loading", async () => {
+    const loading = true;
+    const wrapper = createWrapper(loading);
+    await Vue.nextTick();
+    expect(wrapper.find('.v-progress-circular').exists()).toBe(true);
+  });
+
+  it("error", async () => {
+    const wrapper = createWrapper();
+    wrapper.setData({
+      error: true
+    });
+    await Vue.nextTick();
+    expect(wrapper.text()).toContain("Error: cannot load data");
+  });
+
+  it("none", async () => {
+    const wrapper = createWrapper();
+    wrapper.setData({
+      allMaps: { edges: [] }
+    });
+    await Vue.nextTick();
+    expect(wrapper.text()).toContain("Empty. No maps are found.");
+  });
+});
--- tests/unit/views/Maps.spec.js
+++ tests/unit/views/Maps.spec.js
@@ -4,7 +4,6 @@ import Vuetify from "vuetify";
 import { mount, createLocalVue } from "@vue/test-utils";
 
 import Maps from "@/views/Maps.vue";
-import MapItemCard from "@/components/MapItemCard.vue";
 
 import router from "@/router";
 
@@ -18,47 +17,11 @@ describe("Maps.vue", () => {
     return mount(Maps, {
       localVue,
       router,
-      mocks: {
-        $apollo: {
-          queries: {
-            allMaps: {
-              loading: loading
-            }
-          }
-        }
-      },
       stubs: {
-        MapItemCard: true,
-        MapAddForm: true
+        MapList: true
       }
     });
   }
-
-  const allMaps = {
-    edges: [
-      {
-        node: {
-          id: "TWFwOjEwMTM=",
-          mapId: "1013",
-          name: "lat20200201"
-        }
-      },
-      {
-        node: {
-          id: "TWFwOjEwMTI=",
-          mapId: "1012",
-          name: "lat20200120"
-        }
-      },
-      {
-        node: {
-          id: "TWFwOjEwMDE=",
-          mapId: "1001",
-          name: "lat20190213"
-        }
-      }
-    ]
-  };
 
   beforeEach(function() {
     localVue = createLocalVue();
@@ -66,35 +29,8 @@ describe("Maps.vue", () => {
 
   it("match snapshot", async () => {
     const wrapper = createWrapper();
-    wrapper.setData({
-      allMaps: allMaps
-    });
     await Vue.nextTick();
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  it("loading", async () => {
-    const loading = true;
-    const wrapper = createWrapper(loading);
-    await Vue.nextTick();
-    expect(wrapper.find('.v-progress-circular').exists()).toBe(true);
-  });
-
-  it("error", async () => {
-    const wrapper = createWrapper();
-    wrapper.setData({
-      error: true
-    });
-    await Vue.nextTick();
-    expect(wrapper.text()).toContain("Error: cannot load data");
-  });
-
-  it("none", async () => {
-    const wrapper = createWrapper();
-    wrapper.setData({
-      allMaps: { edges: [] }
-    });
-    await Vue.nextTick();
-    expect(wrapper.text()).toContain("Empty. No maps are found.");
-  });
 });
