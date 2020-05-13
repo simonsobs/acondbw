a5590156f81ffb4274deefdfe8621970815063f1
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-13 17:42:05 -0400

    replace mapFilePath with pathId

--- src/components/MapAddForm.vue
+++ src/components/MapAddForm.vue
@@ -224,7 +224,7 @@ export default {
                   ok
                   mapFilePath {
                     id
-                    mapFilePathId
+                    pathId
                     path
                     note
                   }
