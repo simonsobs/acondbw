7e299c5d99da94310dbabb3faafe184f3d5663e1
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-14 13:30:53 -0400

    rename the fragment map mapFragment

--- src/components/MapEditForm.vue
+++ src/components/MapEditForm.vue
@@ -208,7 +208,7 @@ export default {
               updateMap(productId: $productId, input: $input) {
                 ok
                 map {
-                  ...map
+                  ...mapFragment
                 }
               }
             }
