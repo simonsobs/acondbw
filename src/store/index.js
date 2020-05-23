d3555b16f58408fe31fcbf6b1b839664f14a3019
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-23 15:29:17 -0400

    use Vuex for snackbar

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,20 @@ Vue.use(Vuex);
 
 const state = {
   example: "abc",
+  snackbar: false,
+  snackbarMessage: null
 };
 
 const mutations = {
   set_example(state, example) {
     state.example = example;
+  },
+  set_snackbar_message(state, message) {
+    state.snackbarMessage = message;
+    state.snackbar = true;
+  },
+  set_snackbar(state, snackbar) {
+    state.snackbar = snackbar;
   },
 };
 
@@ -17,6 +26,12 @@ const actions = {
   loadExample({ commit }) {
     const example = "123";
     commit("set_example", example);
+  },
+  snackbarMessage({ commit }, message) {
+    commit("set_snackbar_message", message);
+  },
+  closeSnackbar({ commit }) {
+    commit("set_snackbar", false);
   },
 };
 
