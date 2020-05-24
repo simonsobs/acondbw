3b453be811a73579fb5c2502737a9c8d1f4d6387
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-24 01:35:57 -0400

    configure jest-transform-stub

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "\\.(gql|graphql)$": "./node_modules/jest-transform-graphql"
     /* ... */
   },
+  "moduleNameMapper": {
+    "\\.(css|less)$": "jest-transform-stub"
+  },
   collectCoverage: true,
   collectCoverageFrom: ["**/src/**", '**/*.{vue}', '!**/node_modules/**', '!<rootDir>/dist/**',
                         '!<rootDir>/src/plugins/**', '!<rootDir>/tests/unit/**',
