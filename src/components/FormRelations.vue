7e8462399e2e23817d0102c0339294be62be934c
Tai Sakuma <tai.sakuma@gmail.com>
2020-06-29 15:42:20 -0400

    add delete field button

--- src/components/FormRelations.vue
+++ src/components/FormRelations.vue
@@ -11,31 +11,49 @@
       </v-tooltip>
     </v-row>
     <v-row class="mx-0 mb-3 px-0" v-for="(r, i) in relations" :key="i">
-      <v-col cols="12" md="4">
-        <v-autocomplete
-          label="Relation type"
-          :items="relationTypeItems"
-          clearable
-          v-model="r.typeId"
-        ></v-autocomplete>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-autocomplete
-          label="Product type"
-          :items="productTypeItems"
-          clearable
-          v-model="r.productTypeId"
-        ></v-autocomplete>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-autocomplete
-          label="Product"
-          :items="productTypeMap[r.productTypeId]"
-          clearable
-          hide-no-data
-          v-model="r.productId"
-        ></v-autocomplete>
-      </v-col>
+      <v-card outlined width="100%">
+        <v-container class="py-0">
+          <v-row class="py-0">
+            <v-col cols="12" md="4">
+              <v-autocomplete
+                label="Relation type"
+                :items="relationTypeItems"
+                clearable
+                v-model="r.typeId"
+              ></v-autocomplete>
+            </v-col>
+            <v-col cols="12" md="3">
+              <v-autocomplete
+                label="Product type"
+                :items="productTypeItems"
+                clearable
+                v-model="r.productTypeId"
+              ></v-autocomplete>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-autocomplete
+                label="Product"
+                :items="productTypeMap[r.productTypeId]"
+                clearable
+                hide-no-data
+                v-model="r.productId"
+              ></v-autocomplete>
+            </v-col>
+            <v-col align-self="center" cols="1">
+              <v-row justify="end">
+                <v-tooltip bottom open-delay="800">
+                  <template v-slot:activator="{ on }">
+                    <v-btn icon @click="deleteField(i)" v-on="on">
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Delete the field</span>
+                </v-tooltip>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
     </v-row>
     <v-btn color="secondary" outlined text class="mx-2" @click="addField()">Add a field</v-btn>
   </v-container>
@@ -68,6 +86,9 @@ export default {
   methods: {
     addField() {
       this.relations.push({ ...formRelationDefault });
+    },
+    deleteField(i) {
+      this.relations.splice(i, 1);
     },
     refetch() {
       Object.values(this.$apollo.queries).forEach(query => query.refetch());
