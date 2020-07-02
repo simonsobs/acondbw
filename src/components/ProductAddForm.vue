97bc9b9a9eda367803fff203893c2607aa8ae7e7
Tai Sakuma <tai.sakuma@gmail.com>
2020-07-02 17:26:27 -0400

    remove unused tags

--- src/components/ProductAddForm.vue
+++ src/components/ProductAddForm.vue
@@ -89,7 +89,6 @@
             </v-row>
             <v-divider></v-divider>
           </v-container>
-          <v-card-text></v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="secondary" text @click="close()">Cancel</v-btn>
