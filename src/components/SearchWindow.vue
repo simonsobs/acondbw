1ed9424a9637fd8b65996e976479f1377aece3d1
Tai Sakuma <tai.sakuma@gmail.com>
2020-08-10 13:52:35 -0400

    add SearchWindow

---
+++ src/components/SearchWindow.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-autocomplete
+    class="search-window"
+    outlined
+    rounded
+    dense
+    flat
+    hide-details
+    solo
+    prepend-inner-icon="search"
+    placeholder="Type a product name"
+    :items="items"
+    v-model="value"
+    @input="input"
+    @focus="focus"
+  ></v-autocomplete>
+</template>
+
+<script>
+import State from "@/utils/LoadingState.js";
+import DevToolLoadingStateOverridingMenu from "@/components/DevToolLoadingStateOverridingMenu";
+
+import QueryForSearchWindow from "@/graphql/QueryForSearchWindow.gql";
+
+export default {
+  name: "SearchWindow",
+  components: {
+    DevToolLoadingStateOverridingMenu
+  },
+  data: () => ({
+    s: 0,
+    value: null,
+    edges: null,
+    devtoolState: null,
+    State: State
+  }),
+  apollo: {
+    edges: {
+      query: QueryForSearchWindow,
+      update: function(data) {
+        return data.allProducts.edges;
+      },
+      result(result) {
+        console.log(result);
+      }
+    }
+  },
+  computed: {
+    items() {
+      return this.edges
+        ? this.edges.map(edge => ({
+            text: edge.node.name + " (" + edge.node.type_.singular + ")",
+            value: edge.node
+          }))
+        : null;
+    }
+  },
+  methods: {
+    focus() {
+      this.s += 1;
+    },
+    input(value) {
+      if (!value.type_) {
+        return;
+      }
+      this.$router.push({
+        name: "ProductItem",
+        params: { productTypeName: value.type_.name, name: value.name }
+      });
+      this.$nextTick(() => {
+        this.value = null;
+      });
+    }
+  }
+};
+</script>
+
