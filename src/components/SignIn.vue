1f90bf44721a2c588b83bcac268eae5dff1f1c37
Tai Sakuma <tai.sakuma@gmail.com>
2020-08-25 13:21:56 -0400

    add a dialog in SignIn

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,10 +1,28 @@
 <template>
-  <v-btn outlined>Sign In</v-btn>
+  <v-dialog v-model="dialog" max-width="500">
+    <template v-slot:activator="{ on }">
+      <v-btn outlined v-on="on">Sign In</v-btn>
+    </template>
+    <v-card>
+      <v-card-title>Sign In</v-card-title>
+      <v-card-actions>
+        <v-btn block outlined>
+          <v-icon left>mdi-github</v-icon>Sign In with GitHub
+        </v-btn>
+      </v-card-actions>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="secondary" text @click="dialog=false">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
   name: "SignIn",
-  data: () => ({})
+  data: () => ({
+    dialog: false
+  })
 };
 </script>
