8ff2e7110e31d6f88e7640f39d7feffe338067d3
Tai Sakuma <tai.sakuma@gmail.com>
2020-08-27 12:53:04 -0400

    add path /signin

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
+import SignIn from "@/views/SignIn.vue";
 import ProductTop from "@/views/ProductTop.vue";
 import ProductList from "@/views/ProductList.vue";
 import ProductItem from "@/views/ProductItem.vue";
@@ -12,6 +13,11 @@ const routes = [
     path: "/",
     name: "home",
     component: Home,
+  },
+  {
+    path: "/signin",
+    name: "SignIn",
+    component: SignIn,
   },
   {
     path: "/about",
