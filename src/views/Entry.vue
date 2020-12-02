b7bb204089951480a8d7ec3f2c8893f7bf37798a
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-02 17:49:49 -0500

    replace SignIn with SignInCard in Entry

--- src/views/Entry.vue
+++ src/views/Entry.vue
@@ -2,17 +2,21 @@
   <v-container fill-height fluid>
     <v-row align="center" justify="center">
       <v-col cols="6">
-          <v-container fill-height fluid>
-    <v-row align="center" justify="center">
-      <div class="text-h3">
-        {{ title }}
-      </div>
-    </v-row>
-          </v-container>
+        <v-container fill-height fluid>
+          <v-row align="center" justify="center">
+            <div class="text-h3">
+              {{ title }}
+            </div>
+          </v-row>
+        </v-container>
         <!-- <dashboard></dashboard> -->
       </v-col>
       <v-col cols="6">
-        <sign-in></sign-in>
+        <v-container fill-height fluid>
+          <v-row align="center" justify="center">
+            <sign-in-card></sign-in-card>
+          </v-row>
+        </v-container>
       </v-col>
     </v-row>
   </v-container>
@@ -21,24 +25,23 @@
 <script>
 import WebConfig from "@/graphql/site/WebConfig.gql";
 
-import SignIn from "@/views/auth/SignIn";
+import SignInCard from "@/components/auth/SignInCard";
 import Dashboard from "@/components/product/Dashboard";
 
 export default {
   name: "Home",
   components: {
-    SignIn,
+    SignInCard,
     Dashboard,
   },
   apollo: {
     title: {
       query: WebConfig,
-      update: data =>
-        data.webConfig ? data.webConfig.toolbarTitle : null,
+      update: (data) => (data.webConfig ? data.webConfig.toolbarTitle : null),
       result(result) {
         this.error = result.error ? result.error : null;
-      }
-    }
+      },
+    },
   },
 };
 </script>
