26cb0469ea49138c6cf5580768bc666e2cfbaf60
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-03 13:51:00 -0500

    update tests for router

--- tests/unit/router.spec.js
+++ tests/unit/router.spec.js
@@ -1,7 +1,10 @@
 import moxios from "moxios";
 import router from "@/router/index.js";
 
-describe("About.vue", () => {
+import store from "@/store";
+jest.mock("@/store")
+
+describe("router", () => {
   const ENV_ORG = process.env;
   const ROUTER_HISTORY_CURRNT_ORG = router.history.current;
 
@@ -12,6 +15,8 @@ describe("About.vue", () => {
     // But instead here, only the current and pending are set.
     router.history.current = { ...ROUTER_HISTORY_CURRNT_ORG };
     router.history.pending = null;
+
+    store.state = { auth : { token : null } };
   });
 
   afterEach(() => {
@@ -21,6 +26,19 @@ describe("About.vue", () => {
   });
 
   it("test /", async () => {
+    store.state.auth.token = "xyz";
+    try {
+      await router.push("/");
+    } catch (err) {
+      // Error: Redirected when going from "/" to "/" via a navigation guard.
+      // https://stackoverflow.com/questions/62223195/vue-router-uncaught-in-promise-error-redirected-from-login-to-via-a
+    }
+    const current = router.history.current;
+    expect(current.name).toBe("Dashboard");
+    expect(current.path).toBe("/dashboard");
+  });
+
+  it("test / not sigend in", async () => {
     await router.push("/");
     const current = router.history.current;
     expect(current.name).toBe("Entry");
@@ -36,6 +54,7 @@ describe("About.vue", () => {
   });
 
   it("test /map/item/:name", async () => {
+    store.state.auth.token = "xyz";
     await router.push("/map/item/map001");
     const current = router.history.current;
     expect(current.matched.length).toBe(2);
@@ -49,6 +68,7 @@ describe("About.vue", () => {
   });
 
   it("test /map", async () => {
+    store.state.auth.token = "xyz";
     await router.push("/map");
     const current = router.history.current;
     expect(current.matched.length).toBe(2);
