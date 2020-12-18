0738f3f88408be59e8a4d6a21ddc6282b2e4d852
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-18 10:27:34 -0500

    add three views for admin

---
+++ src/views/admin-token/AdminAppToken.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <admin-app-token-card></admin-app-token-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AdminAppTokenCard from "@/components/admin-token/AdminAppTokenCard";
+
+export default {
+  name: "AdminAppToken",
+  components: {
+    AdminAppTokenCard
+  },
+  data: () => ({}),
+};
+</script>
---
+++ src/views/admin-token/AdminAppTokenError.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-card flat min-width="250px">
+        <v-card-title>Admin App Unsuccessful</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn outlined :to="{ name: 'AdminAppToken' }">Try again</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "AdminAppTokenError",
+};
+</script>
