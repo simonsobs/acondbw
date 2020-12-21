af4cec25d1b4a66fdc8229ab5477688db800d205
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-21 10:33:53 -0500

    make copies of Frame and Navigation

---
+++ src/components/layout/FrameAdmin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <navigation-admin></navigation-admin>
+    </v-navigation-drawer>
+    <app-bar v-on:drawer="drawer = !drawer"></app-bar>
+  </div>
+</template>
+
+<script>
+import AppBar from "./AppBar";
+import NavigationAdmin from "./NavigationAdmin";
+
+export default {
+  name: "FrameAdmin",
+  components: {
+    AppBar,
+    NavigationAdmin,
+  },
+  data: () => ({
+    drawer: true,
+  }),
+};
+</script>
