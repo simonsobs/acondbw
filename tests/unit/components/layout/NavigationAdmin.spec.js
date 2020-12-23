38a83e3e086f29928a7ad33ddbf4e82ba142dd49
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-23 18:25:26 -0500

    add tests for NavigationAdmin

---
+++ tests/unit/components/layout/NavigationAdmin.spec.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Vuetify from "vuetify";
+import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
+
+import NavigationAdmin from "@/components/layout/NavigationAdmin";
+import router from "@/router";
+
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+describe("App.vue", () => {
+  let localVue;
+  let vuetify;
+  let wrapper;
+
+  beforeEach(() => {
+    localVue = createLocalVue();
+    vuetify = new Vuetify();
+    wrapper = shallowMount(NavigationAdmin, {
+      localVue,
+      vuetify,
+      router,
+    });
+  });
+
+  it("match snapshot", () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
