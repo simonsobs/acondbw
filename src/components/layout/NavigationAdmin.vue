5222f171086a23380dde8e3be1d0afdcfa395364
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-23 18:21:25 -0500

    update NavigationAdmin

--- src/components/layout/NavigationAdmin.vue
+++ src/components/layout/NavigationAdmin.vue
@@ -1,89 +1,34 @@
 <template>
-  <div class="navigation" style="position: relative;">
-    <div v-if="state == State.LOADED">
-      <v-list shaped>
-        <v-list-item
-          link
-          router
-          v-for="edge in edges"
-          :key="edge.node.typeId"
-          :to="'/' + edge.node.name"
-        >
-          <v-list-item-action>
-            <v-icon v-text="edge.node.icon"></v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="edge.node.plural" class="capitalize"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </div>
-    <v-alert
-      v-else-if="state == State.EMPTY"
-      outlined
-      dense
-      type="info"
-      class="ma-2"
-    >No product types are defined.</v-alert>
-    <div v-else-if="state == State.LOADING" class="mx-2 pt-5">
-      <v-progress-circular indeterminate :size="26" color="grey"></v-progress-circular>
-    </div>
-    <v-alert v-else-if="state == State.ERROR" outlined dense type="error" class="ma-2">{{ error }}</v-alert>
-    <dev-tool-loading-state-overriding-menu @state="devtoolState = $event"></dev-tool-loading-state-overriding-menu>
+  <div class="navigation" style="position: relative">
+    <v-list shaped>
+      <v-list-item
+        link
+        router
+        v-for="(link, index) in links"
+        :key="index"
+        :to="link.to"
+      >
+        <v-list-item-action>
+          <v-icon v-text="link.icon"></v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title
+            v-text="link.title"
+            class="capitalize"
+          ></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
 <script>
-import ALL_PRODUCTS_TYPES from "@/graphql/product/AllProductTypes.gql";
-
-import State from "@/utils/LoadingState.js";
-import DevToolLoadingStateOverridingMenu from "@/components/utils/DevToolLoadingStateOverridingMenu";
-
 export default {
   name: "NavigationAdmin",
-  components: {
-    DevToolLoadingStateOverridingMenu
-  },
   data: () => ({
     edges: null,
-    error: null,
-    devtoolState: null,
-    State: State
+    links: [{ title: "admin app tokens", to: "to", icon: "mdi-key"}],
   }),
-  apollo: {
-    edges: {
-      query: ALL_PRODUCTS_TYPES,
-      update: data =>
-        data.allProductTypes ? data.allProductTypes.edges : null,
-      result(result) {
-        this.error = result.error ? result.error : null;
-      }
-    }
-  },
-  computed: {
-    state() {
-      if (this.devtoolState) {
-        return this.devtoolState;
-      }
-
-      if (this.loading) {
-        return State.LOADING;
-      } else if (this.error) {
-        return State.ERROR;
-      } else if (this.edges) {
-        if (this.edges.length) {
-          return State.LOADED;
-        } else {
-          return State.EMPTY;
-        }
-      } else {
-        return State.NONE;
-      }
-    },
-    loading() {
-      return this.$apollo.queries.edges.loading;
-    }
-  }
 };
 </script>
 
