19b25d17f6cd7983c1b4d573ab5b26bdd10d2a73
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-28 11:28:18 -0500

    add a new view OAuthRedirect

---
+++ src/views/auth/OAuthRedirect.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-progress-circular
+        indeterminate
+        :size="18"
+        :width="3"
+        color="grey"
+      ></v-progress-circular>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { validateState } from "@/utils/auth.js";
+
+export default {
+  name: "OAuthRedirect",
+  data: () => ({}),
+  methods: {
+    async main() {
+      let state = this.$route.query.state;
+      if (!state) {
+        this.$router.push({ path: "/" });
+        return;
+      }
+
+      if (!validateState(state)) {
+        this.$router.push({ path: "/" });
+        return;
+      }
+
+      state = JSON.parse(atob(state));
+      // e.g.,
+      //   state = {
+      //     redirect: { name: "Auth" },
+      //     code: "XXXXXXXX",
+      //   };
+
+      const redirect = { ...state.redirect, query: this.$route.query };
+      // e.g.,
+      //   redirect = {
+      //     name: "Auth",
+      //     query: {
+      //       code: "XXXXXXXX",
+      //       state: "XXXXXXXXXXXXXXXX"
+      //     }
+      //   }
+
+      this.$router.push(redirect);
+      return;
+    },
+  },
+  mounted: async function () {
+    this.main();
+  },
+};
+</script>
