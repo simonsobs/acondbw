34e6e7676dec3feb706b652706f5c7a15255fbe9
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-28 17:17:23 -0500

    update tests for AdminAppTokenCard

--- tests/unit/components/admin-token/AdminAppTokenCard.spec.js
+++ tests/unit/components/admin-token/AdminAppTokenCard.spec.js
@@ -1,13 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Vuex from "vuex";
 import Vuetify from "vuetify";
 import { mount, createLocalVue } from "@vue/test-utils";
 
 import AdminAppTokenCard from "@/components/admin-token/AdminAppTokenCard.vue";
 import router from "@/router";
 
-import { requestAuthForAdminApp } from "@/utils/admin-token.js";
-jest.mock("@/utils/admin-token.js");
+import store from "@/store";
+jest.mock("@/store");
+
+import { requestAuth } from "@/utils/auth.js";
+jest.mock("@/utils/auth.js");
 
 Vue.use(Vuetify);
 Vue.use(VueRouter);
@@ -15,20 +19,32 @@ Vue.use(VueRouter);
 describe("AdminAppTokenCard.vue", () => {
   let localVue;
   let vuetify;
+  let actions;
+  let store_;
 
   function createWrapper() {
     let wrapper = mount(AdminAppTokenCard, {
       localVue,
       router,
       vuetify,
+      store: store_,
     });
     return wrapper;
   }
 
   beforeEach(function () {
-    requestAuthForAdminApp.mockClear();
+    requestAuth.mockClear();
     localVue = createLocalVue();
+    localVue.use(Vuex);
     vuetify = new Vuetify();
+    actions = {
+      clearAuthError: jest.fn(),
+    };
+    store_ = new Vuex.Store({
+      actions,
+    });
+
+    store.state = { auth : { token : "XXXXXXXXXX" } }; // mock store in "@/src/router/index.js"
   });
 
   it("match snapshot", async () => {
@@ -48,21 +64,23 @@ describe("AdminAppTokenCard.vue", () => {
     const wrapper = createWrapper();
     await Vue.nextTick();
     wrapper.vm.requestAuth();
-    expect(requestAuthForAdminApp.mock.calls.length).toBe(1);
+    expect(actions.clearAuthError.mock.calls.length).toBe(1);
+    expect(requestAuth.mock.calls.length).toBe(1);
     expect(wrapper.vm.loading).toBeTruthy();
   });
 
   it("requestAuth error", async () => {
     const wrapper = createWrapper();
     await Vue.nextTick();
-    requestAuthForAdminApp.mockImplementation(() => {
+    requestAuth.mockImplementation(() => {
       throw new Error();
     });
     wrapper.vm.requestAuth();
     await Vue.nextTick();
     await Vue.nextTick();
-    expect(requestAuthForAdminApp.mock.calls.length).toBe(1);
+    expect(actions.clearAuthError.mock.calls.length).toBe(1);
+    expect(requestAuth.mock.calls.length).toBe(1);
     expect(wrapper.vm.loading).toBeFalsy();
-    expect(router.history.current.name).toBe("AdminAppTokenError")
+    expect(router.history.current.name).toBe("AdminAppTokenError");
   });
 });
