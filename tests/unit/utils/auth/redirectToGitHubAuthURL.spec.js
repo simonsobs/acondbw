af3210ad7e4807788e1befc260b452ace24b3091
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-29 16:52:25 -0500

    format source

--- tests/unit/utils/auth/redirectToGitHubAuthURL.spec.js
+++ tests/unit/utils/auth/redirectToGitHubAuthURL.spec.js
@@ -31,7 +31,7 @@ describe("redirectToGitHubAuthURL", () => {
     const url = window.location.href.split("?")[0];
     const query = getJsonFromUrl(window.location.href);
     expect(url).toBe("https://github.com/login/oauth/authorize");
-    expect(query).toMatchSnapshot()
+    expect(query).toMatchSnapshot();
     expect(JSON.parse(localStorage[AUTH_STATE])).toEqual(query.state);
   });
 
@@ -49,25 +49,27 @@ describe("redirectToGitHubAuthURL", () => {
 function getJsonFromUrl(url) {
   var question = url.indexOf("?");
   var hash = url.indexOf("#");
-  if(hash==-1 && question==-1) return {};
-  if(hash==-1) hash = url.length;
-  var query = question==-1 || hash==question+1 ? url.substring(hash) : 
-  url.substring(question+1,hash);
+  if (hash == -1 && question == -1) return {};
+  if (hash == -1) hash = url.length;
+  var query =
+    question == -1 || hash == question + 1
+      ? url.substring(hash)
+      : url.substring(question + 1, hash);
   var result = {};
-  query.split("&").forEach(function(part) {
-    if(!part) return;
+  query.split("&").forEach(function (part) {
+    if (!part) return;
     part = part.split("+").join(" "); // replace every + with space, regexp-free version
     var eq = part.indexOf("=");
-    var key = eq>-1 ? part.substr(0,eq) : part;
-    var val = eq>-1 ? decodeURIComponent(part.substr(eq+1)) : "";
+    var key = eq > -1 ? part.substr(0, eq) : part;
+    var val = eq > -1 ? decodeURIComponent(part.substr(eq + 1)) : "";
     var from = key.indexOf("[");
-    if(from==-1) result[decodeURIComponent(key)] = val;
+    if (from == -1) result[decodeURIComponent(key)] = val;
     else {
-      var to = key.indexOf("]",from);
-      var index = decodeURIComponent(key.substring(from+1,to));
-      key = decodeURIComponent(key.substring(0,from));
-      if(!result[key]) result[key] = [];
-      if(!index) result[key].push(val);
+      var to = key.indexOf("]", from);
+      var index = decodeURIComponent(key.substring(from + 1, to));
+      key = decodeURIComponent(key.substring(0, from));
+      if (!result[key]) result[key] = [];
+      if (!index) result[key].push(val);
       else result[key][index] = val;
     }
   });
