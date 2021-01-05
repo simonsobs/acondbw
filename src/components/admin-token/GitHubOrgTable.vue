4ba25b726539e76267884ab5d295edffce302390
Tai Sakuma <tai.sakuma@gmail.com>
2021-01-05 14:29:14 -0500

    update GitHubOrgTable

--- src/components/admin-token/GitHubOrgTable.vue
+++ src/components/admin-token/GitHubOrgTable.vue
@@ -8,6 +8,38 @@
       >
         <template v-slot:top>
           <v-toolbar flat>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialogAdd" max-width="500px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon v-bind="attrs" v-on="on">
+                  <v-icon>mdi-plus-thick</v-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>Add a GitHub organization</v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="addLogin"
+                          label="GitHub organization account name"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="secondary" text @click="closeAdd"
+                    >Cancel</v-btn
+                  >
+                  <v-btn color="primary" text @click="addItem"
+                    >OK</v-btn
+                  >
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
             <v-dialog v-model="dialogDelete" max-width="500px">
               <v-card>
                 <v-card-title class="headline">Remove</v-card-title>
@@ -52,17 +84,20 @@
 
 import ALL_GIT_HUB_ORGS from "@/graphql/admin-token/AllGitHubOrgs.gql";
 import DELETE_GITHUB_ORG from "@/graphql/admin-token/DeleteGitHubOrg.gql";
+import ADD_GITHUB_ORG from "@/graphql/admin-token/AddGitHubOrg.gql";
 
 export default {
   name: "GitHubOrgTable",
   data: () => ({
     allGitHubOrgs: null,
     allGitHubOrgsHeaders: [
-      { text: "", value: "node.avatarUrl" },
+      { text: "", value: "node.avatarUrl", align: "start" },
       { text: "Org", value: "node.login" },
       { text: "Number of members", value: "node.memberships.totalCount" },
-      { text: "", value: "actions", sortable: false },
+      { text: "", value: "actions", sortable: false, align: "end"},
     ],
+    dialogAdd: false,
+    addLogin: "",
     dialogDelete: false,
     deleteLogin: null,
     alert: false,
@@ -74,6 +109,24 @@ export default {
     },
   },
   methods: {
+    closeAdd() {
+      this.dialogAdd = false;
+      this.addLogin = "";
+    },
+    async addItem() {
+      try {
+        const { data } = await this.$apollo.mutate({
+          mutation: ADD_GITHUB_ORG,
+          variables: { login: this.addLogin },
+        });
+        this.$apollo.queries.allGitHubOrgs.refetch();
+        this.$store.dispatch("apolloMutationCalled");
+        this.$store.dispatch("snackbarMessage", "Added");
+      } catch (error) {
+        this.error = error;
+      }
+      this.closeAdd();
+    },
     deleteItem(item) {
       const index = this.allGitHubOrgs.edges.indexOf(item);
       this.deleteLogin = this.allGitHubOrgs.edges[index].node.login;
