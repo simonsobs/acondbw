e451a1eb2722309bb7c12ee1692084165f89edad
Tai Sakuma <tai.sakuma@gmail.com>
2021-01-05 16:40:13 -0500

    remove old code from Scratch

--- src/views/admin-token/Scratch.vue
+++ src/views/admin-token/Scratch.vue
@@ -1,11 +1,6 @@
 <template>
   <v-container fill-height fluid>
     <v-card flat>
-      <v-card-actions>
-        <v-btn block outlined @click="updateOrgMemberList">
-          <v-icon left>mdi-github</v-icon>Update org member list
-        </v-btn>
-      </v-card-actions>
       <v-card>
         <v-card-title>Tokens with org access</v-card-title>
         <v-card-text>
@@ -54,9 +49,7 @@
 </template>
 
 <script>
-// import ALL_GITHUB_TOKENS from "@/graphql/admin-token/AllGitHubTokens.gql";
 import ALL_GIT_HUB_TOKENS_WITH_ORG_ACCESS from "@/graphql/admin-token/AllGitHubTokensWithOrgAccess.gql";
-import UPDATE_GITHUB_ORG_MEMBER_LIST from "@/graphql/admin-token/UpdateGitHubOrgMemberLists.gql";
 
 import GitHubOrgTable from "@/components/admin-token/GitHubOrgTable";
 import GitHubUserTable from "@/components/admin-token/GitHubUserTable";
@@ -81,17 +74,6 @@ export default {
   apollo: {
     allGitHubTokens: {
       query: ALL_GIT_HUB_TOKENS_WITH_ORG_ACCESS,
-    },
-  },
-  methods: {
-    async updateOrgMemberList() {
-      const { data } = await this.$apollo.mutate({
-        mutation: UPDATE_GITHUB_ORG_MEMBER_LIST,
-      });
-    },
-    deleteToken(item) {
-      // https://vuetifyjs.com/en/components/data-tables/#crud-actions
-      console.log(item);
     },
   },
 };
