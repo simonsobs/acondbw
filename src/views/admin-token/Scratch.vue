6b807ec012a88b0cefea50bcd55609558c5d9368
Tai Sakuma <tai.sakuma@gmail.com>
2021-01-06 11:20:06 -0500

    update Scratch

--- src/views/admin-token/Scratch.vue
+++ src/views/admin-token/Scratch.vue
@@ -2,20 +2,21 @@
   <v-container fill-height fluid>
     <v-row align="center" justify="center">
       <v-card flat>
-        <v-card>
+        <div class="text-h4">Admin</div>
+        <v-card class="my-2">
           <v-card-title>Tokens with org access</v-card-title>
           <v-card-text>
             <git-hub-token-table></git-hub-token-table>
           </v-card-text>
         </v-card>
-        <v-card>
+        <v-card class="my-2">
           <v-card-title>Orgnizations</v-card-title>
           <v-card-text>
             <git-hub-org-table></git-hub-org-table>
           </v-card-text>
         </v-card>
-        <v-card>
-          <v-card-title>Users</v-card-title>
+        <v-card class="my-2">
+          <v-card-title>Organization members</v-card-title>
           <v-card-text>
             <git-hub-user-table></git-hub-user-table>
           </v-card-text>
