1c8f229f32ec6ff3faf2d31c16eb744a7200e215
Tai Sakuma <tai.sakuma@gmail.com>
2021-03-02 18:02:03 -0500

    update layout of ProductAddForm

--- src/components/product/ProductAddForm.vue
+++ src/components/product/ProductAddForm.vue
@@ -1,108 +1,141 @@
 <template>
-  <div class="product-add-form" style="position: relative;">
-    <v-container>
-      <v-card outlined max-width="980px" class="mx-auto my-5">
-        <v-card-title
-          v-if="loaded"
-          class="headline primary--text"
-        >Add {{ productType.indefArticle }} {{ productType.singular }}</v-card-title>
-        <v-alert v-if="error" type="error">{{ error }}</v-alert>
-        <v-form v-if="loaded" ref="form" v-model="valid">
-          <v-divider></v-divider>
-          <v-container fluid class="px-0">
-            <v-row class="ma-0 px-0">
-              <v-col order="1" cols="12" md="4">
-                <v-text-field
-                  label="Name*"
-                  required
-                  :hint="'Name of the ' + productType.singular + '. This field cannot be changed later.'"
-                  persistent-hint
-                  v-model="form.name"
-                  :rules="nameRules"
-                ></v-text-field>
-              </v-col>
-              <v-col order="3" cols="6" md="4">
-                <v-text-field-with-date-picker
-                  label="Date produced (YYYY-MM-DD)*"
-                  :hint="'The date on which the ' + productType.singular + ' was produced, e.g., 2020-05-06. This field cannot be changed later.'"
-                  v-model="form.dateProduced"
-                  :rules="requiredRules"
-                ></v-text-field-with-date-picker>
-              </v-col>
-              <v-col order="4" cols="6" md="4">
-                <v-text-field
-                  label="Produced by*"
-                  required
-                  :hint="'The person or group that produced the ' + productType.singular + ', e.g. pwg-xxx. This field cannot be changed later.'"
-                  persistent-hint
-                  v-model="form.producedBy"
-                  :rules="requiredRules"
-                ></v-text-field>
-              </v-col>
-              <v-col order="4" cols="6" offset-md="4" md="4">
-                <v-text-field
-                  label="Contact*"
-                  required
-                  :hint="'A person or group that can be contacted for questions or issues about the ' + productType.singular + '.'"
-                  persistent-hint
-                  v-model="form.contact"
-                  :rules="requiredRules"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row class="mx-0 mb-3 px-0">
-              <v-col cols="12" md="8" offset-md="4">
-                <v-textarea
-                  label="Paths"
-                  hint="A path per line. e.g., nersc:/go/to/my/product_v3"
-                  rows="2"
-                  persistent-hint
-                  v-model="form.paths"
-                ></v-textarea>
-              </v-col>
-            </v-row>
-            <v-row class="mx-0 mb-3 px-0">
-              <v-col cols="12" md="8" offset-md="4">
-                <v-textarea
-                  label="Note"
-                  hint="will be parsed as Markdown"
-                  persistent-hint
-                  rows="3"
-                  v-model="form.note"
-                ></v-textarea>
-              </v-col>
-            </v-row>
-            <v-divider></v-divider>
-            <v-row justify="end" class="mx-2 mb-3 px-0">
-              <v-card-text>Relations to other products</v-card-text>
-              <form-relations :relations="form.relations"></form-relations>
-            </v-row>
-            <v-divider></v-divider>
-          </v-container>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="secondary" text @click="close()">Cancel</v-btn>
-            <v-btn color="secondary" text @click="resetForm()">Reset</v-btn>
-            <v-btn color="primary" :disabled="!valid" @click="addProduct()">Add</v-btn>
-          </v-card-actions>
-        </v-form>
-        <div v-else>
-          <v-card outlined>
-            <div v-if="loading" class="mx-4 py-2">
-              <v-progress-circular indeterminate :size="18" :width="3" color="grey"></v-progress-circular>
-            </div>
-            <v-alert v-else-if="queryError" outlined dense type="error" class="ma-2">{{ queryError }}</v-alert>
-            <v-card-text v-else-if="notFound">Not Found</v-card-text>
-          </v-card>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="secondary" text @click="close()">Close</v-btn>
-          </v-card-actions>
+  <v-card
+    outlined
+    max-width="980px"
+    class="product-add-form mx-auto my-5"
+    style="position: relative"
+  >
+    <v-card-title v-if="loaded" class="headline primary--text"
+      >Add {{ productType.indefArticle }}
+      {{ productType.singular }}</v-card-title
+    >
+    <v-alert v-if="error" type="error">{{ error }}</v-alert>
+    <v-form v-if="loaded" ref="form" v-model="valid">
+      <v-divider></v-divider>
+      <v-container fluid class="px-0">
+        <v-row class="ma-0 px-0">
+          <v-col order="1" cols="12" md="4">
+            <v-text-field
+              label="Name*"
+              required
+              :hint="
+                'Name of the ' +
+                productType.singular +
+                '. This field cannot be changed later.'
+              "
+              persistent-hint
+              v-model="form.name"
+              :rules="nameRules"
+            ></v-text-field>
+          </v-col>
+          <v-col order="3" cols="6" md="4">
+            <v-text-field-with-date-picker
+              label="Date produced (YYYY-MM-DD)*"
+              :hint="
+                'The date on which the ' +
+                productType.singular +
+                ' was produced, e.g., 2020-05-06. This field cannot be changed later.'
+              "
+              v-model="form.dateProduced"
+              :rules="requiredRules"
+            ></v-text-field-with-date-picker>
+          </v-col>
+          <v-col order="4" cols="6" md="4">
+            <v-text-field
+              label="Produced by*"
+              required
+              :hint="
+                'The person or group that produced the ' +
+                productType.singular +
+                ', e.g. pwg-xxx. This field cannot be changed later.'
+              "
+              persistent-hint
+              v-model="form.producedBy"
+              :rules="requiredRules"
+            ></v-text-field>
+          </v-col>
+          <v-col order="4" cols="6" offset-md="4" md="4">
+            <v-text-field
+              label="Contact*"
+              required
+              :hint="
+                'A person or group that can be contacted for questions or issues about the ' +
+                productType.singular +
+                '.'
+              "
+              persistent-hint
+              v-model="form.contact"
+              :rules="requiredRules"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row class="mx-0 mb-3 px-0">
+          <v-col cols="12" md="8" offset-md="4">
+            <v-textarea
+              label="Paths"
+              hint="A path per line. e.g., nersc:/go/to/my/product_v3"
+              rows="2"
+              persistent-hint
+              v-model="form.paths"
+            ></v-textarea>
+          </v-col>
+        </v-row>
+        <v-row class="mx-0 mb-3 px-0">
+          <v-col cols="12" md="8" offset-md="4">
+            <v-textarea
+              label="Note"
+              hint="will be parsed as Markdown"
+              persistent-hint
+              rows="3"
+              v-model="form.note"
+            ></v-textarea>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row justify="end" class="mx-2 mb-3 px-0">
+          <v-card-text>Relations to other products</v-card-text>
+          <form-relations :relations="form.relations"></form-relations>
+        </v-row>
+        <v-divider></v-divider>
+      </v-container>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="secondary" text @click="close()">Cancel</v-btn>
+        <v-btn color="secondary" text @click="resetForm()">Reset</v-btn>
+        <v-btn color="primary" :disabled="!valid" @click="addProduct()"
+          >Add</v-btn
+        >
+      </v-card-actions>
+    </v-form>
+    <div v-else>
+      <v-card outlined>
+        <div v-if="loading" class="mx-4 py-2">
+          <v-progress-circular
+            indeterminate
+            :size="18"
+            :width="3"
+            color="grey"
+          ></v-progress-circular>
         </div>
+        <v-alert
+          v-else-if="queryError"
+          outlined
+          dense
+          type="error"
+          class="ma-2"
+          >{{ queryError }}</v-alert
+        >
+        <v-card-text v-else-if="notFound">Not Found</v-card-text>
       </v-card>
-      <dev-tool-loading-state-overriding-menu @state="devtoolState = $event"></dev-tool-loading-state-overriding-menu>
-    </v-container>
-  </div>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="secondary" text @click="close()">Close</v-btn>
+      </v-card-actions>
+    </div>
+    <dev-tool-loading-state-overriding-menu
+      @state="devtoolState = $event"
+    ></dev-tool-loading-state-overriding-menu>
+  </v-card>
 </template>
   
 <script>
@@ -120,7 +153,7 @@ import DevToolLoadingStateOverridingMenu from "@/components/utils/DevToolLoading
 const formRelationDefault = {
   typeId: null,
   productTypeId: null,
-  productId: null
+  productId: null,
 };
 
 const formDefault = {
@@ -130,7 +163,7 @@ const formDefault = {
   producedBy: "",
   paths: "",
   relations: [{ ...formRelationDefault }, { ...formRelationDefault }],
-  note: ""
+  note: "",
 };
 
 export default {
@@ -138,10 +171,10 @@ export default {
   components: {
     VTextFieldWithDatePicker,
     FormRelations,
-    DevToolLoadingStateOverridingMenu
+    DevToolLoadingStateOverridingMenu,
   },
   props: {
-    productTypeId: { required: true }
+    productTypeId: { required: true },
   },
   data() {
     return {
@@ -155,10 +188,10 @@ export default {
       valid: true,
       error: null,
       nameRules: [
-        v => !!v || "This field is required",
-        v => (v || "").indexOf(" ") < 0 || "No spaces are allowed"
+        (v) => !!v || "This field is required",
+        (v) => (v || "").indexOf(" ") < 0 || "No spaces are allowed",
       ],
-      requiredRules: [v => !!v || "This field is required"]
+      requiredRules: [(v) => !!v || "This field is required"],
     };
   },
   computed: {
@@ -187,7 +220,7 @@ export default {
     },
     notFound() {
       return this.state == State.NONE;
-    }
+    },
   },
   apollo: {
     productType: {
@@ -195,7 +228,7 @@ export default {
       variables() {
         return { typeId: this.productTypeId };
       },
-      skip: function() {
+      skip: function () {
         return !this.productTypeId;
       },
       result(result) {
@@ -206,8 +239,8 @@ export default {
         }
 
         this.productType = result.data.productType;
-      }
-    }
+      },
+    },
   },
   watch: {
     devtoolState: function () {
@@ -246,24 +279,24 @@ export default {
           "contact",
           "dateProduced",
           "producedBy",
-          "note"
+          "note",
         ]);
 
         createProductInput.typeId = this.productTypeId;
 
         const paths = this.form.paths
           .split("\n")
-          .map(x => x.trim()) // trim e.g., " /a/b/c " => "/a/b/c"
+          .map((x) => x.trim()) // trim e.g., " /a/b/c " => "/a/b/c"
           .filter(Boolean) // remove empty strings
           .filter((v, i, a) => a.indexOf(v) === i); // unique
 
         createProductInput.paths = paths;
 
         createProductInput.relations = _.filter(
-          _.map(this.form.relations, x =>
+          _.map(this.form.relations, (x) =>
             _.pickBy(_.pick(x, ["typeId", "productId"]), _.identity)
           ),
-          x => _.size(x) == 2
+          (x) => _.size(x) == 2
         );
 
         const data = await this.$apollo.mutate({
@@ -272,11 +305,11 @@ export default {
           update: (cache, { data: { createProduct } }) => {
             try {
               // Delete all cache and dispacth "apolloMutationCalled", which triggers refetch
-              this.$apollo.provider.defaultClient.cache.data.data = {}
+              this.$apollo.provider.defaultClient.cache.data.data = {};
 
-              // The following code was used to update cache, which 
+              // The following code was used to update cache, which
               // is typically a recommended way. But it is commented out
-              // because it is not clear how to systematically update 
+              // because it is not clear how to systematically update
               // all affected cache.
 
               // const data = cache.readQuery({
@@ -292,9 +325,8 @@ export default {
               //   variables: { typeId: this.productTypeId },
               //   data
               // });
-            } catch (error) {
-            }
-          }
+            } catch (error) {}
+          },
         });
         this.$store.dispatch("apolloMutationCalled");
         this.$store.dispatch("snackbarMessage", "Added");
@@ -304,7 +336,7 @@ export default {
         this.error = error;
         this.scrollToTop();
       }
-    }
-  }
+    },
+  },
 };
 </script>
