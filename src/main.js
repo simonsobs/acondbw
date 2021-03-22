a04f447e36ba1046289c51e31756662221975644
Tai Sakuma <tai.sakuma@gmail.com>
2021-03-22 10:05:29 -0400

    use Vuelidate

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { apolloProvider } from "./vue-apollo";
+import Vuelidate from 'vuelidate'
 import InstantSearch from 'vue-instantsearch';
 
+Vue.use(Vuelidate)
 Vue.use(InstantSearch);
 
 Vue.config.productionTip = false;
