b96e49d8da31a7ba963536b873117278da366e7e
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-26 09:35:06 -0400

    add commented out code

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,26 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+
+import colors from "vuetify/lib/util/colors";
+
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: "mdi"
-  }
+    iconfont: "mdi",
+  },
+  theme: {
+    // dark: false,
+    // options: { customProperties: true },
+    // themes: {
+    //   light: {
+    //     primary: colors.teal.base,
+    //     "on-primary": colors.shades.white,
+    //     secondary: "#b0bec5",
+    //     anchor: "#8c9eff",
+    //   },
+    // },
+  },
 });
