5af410197addc840e68adbe0bbe4d2b263de8ffd
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-28 16:11:46 -0400

    update vue router tests

--- tests/unit/router.spec.js
+++ tests/unit/router.spec.js
@@ -45,6 +45,43 @@ describe("router", () => {
     expect(current.path).toBe("/");
   });
 
+  it("test /dashboard", async () => {
+    store.state.auth.isSignedIn = true;
+    await router.push("/dashboard");
+    const current = router.history.current;
+    expect(current.name).toBe("Dashboard");
+    expect(current.path).toBe("/dashboard");
+  });
+
+  it("test /dashboard not sigend in", async () => {
+    try {
+      await router.push("/dashboard");
+    } catch (err) {}
+    const current = router.history.current;
+    expect(current.name).toBe("Entry");
+    expect(current.path).toBe("/");
+  });
+
+  it("test requiresAuth", async () => {
+    try {
+      await router.push("/product/map");
+    } catch (err) {}
+    // not sure how to await for pending to become current
+    const pending = router.history.pending;
+    expect(pending.name).toBe("AccessDenied");
+    expect(pending.path).toBe("/access-denied");
+  });
+
+  it("test requiresAdmin", async () => {
+    store.state.auth.isSignedIn = true;
+    try {
+      await router.push("/admin/log");
+    } catch (err) {}
+    const current = router.history.current;
+    expect(current.name).toBe("AccessDenied");
+    expect(current.path).toBe("/access-denied");
+  });
+
   it("test /about", async () => {
     await router.push("/about");
     const current = router.history.current;
@@ -76,7 +113,7 @@ describe("router", () => {
     expect(current.matched[0].props).toEqual({});
   });
 
-  it("test /map", async () => {
+  it("test /product/map", async () => {
     store.state.auth.isSignedIn = true;
     await router.push("/product/map");
     const current = router.history.current;
