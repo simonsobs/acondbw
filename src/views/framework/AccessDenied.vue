e59110e1e3698e287b950238a1146b65e46b664b
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-28 14:31:33 -0400

    show link to sing-in page if not signed in

--- src/views/framework/AccessDenied.vue
+++ src/views/framework/AccessDenied.vue
@@ -3,7 +3,12 @@
     <v-row align="center" justify="center">
       <v-col class="text-h3 text-center">
         <div>Access Denied</div>
-        <v-btn class="mt-5" outlined to="/">Go to the entry page</v-btn>
+        <v-btn v-if="isSignedIn" class="mt-5" outlined to="/">
+          Go to the entry page
+        </v-btn>
+        <v-btn v-else class="mt-5" outlined :to="{ name: 'SignIn' }">
+          Go to the sign-in page
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -14,6 +19,11 @@ export default {
   name: "AccessDenied",
   data() {
     return {};
+  },
+  computed: {
+    isSignedIn() {
+      return this.$store.state.auth.isSignedIn;
+    },
   },
 };
 </script>
