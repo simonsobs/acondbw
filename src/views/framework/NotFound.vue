d528470b4d866d72a437fc6c79099e886ba40371
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-28 11:37:45 -0400

    replace div with v-col

--- src/views/framework/NotFound.vue
+++ src/views/framework/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fill-height fluid>
     <v-row align="center" justify="center">
-      <div class="display-2 text-center">Not Found (404)</div>
+      <v-col class="display-2 text-center">Not Found (404)</v-col>
     </v-row>
   </v-container>
 </template>
