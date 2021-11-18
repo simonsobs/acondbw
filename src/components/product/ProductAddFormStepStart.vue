0beb52092166cc80afd1250a546527cf3cd33765
Tai Sakuma <tai.sakuma@gmail.com>
2021-11-18 13:58:25 -0500

    remove v-form

--- src/components/product/ProductAddFormStepStart.vue
+++ src/components/product/ProductAddFormStepStart.vue
@@ -1,100 +1,98 @@
 <template>
-  <v-form ref="form">
-    <v-container fluid>
-      <v-alert v-if="error" type="error">{{ error }}</v-alert>
-      <v-row justify="center">
-        <v-col order="1" cols="12" md="10">
-          <v-text-field
-            outlined
-            label="Name*"
-            required
-            :hint="`Name of the ${productType.singular}.`"
-            persistent-hint
-            :error-messages="nameErrors"
-            :value="$v.form.name.$model"
-            @input="debounceInput($v.form.name, $event)"
-            @blur="$v.form.name.$touch()"
-          ></v-text-field>
-        </v-col>
-        <v-col order="3" cols="12" md="10">
-          <v-text-field-with-date-picker
-            outlined
-            label="Date produced (YYYY-MM-DD)*"
-            required
-            :hint="`The date on which the ${productType.singular} was produced, e.g., 2020-05-06. In the current version of the product DB, this field cannot be changed once the ${productType.singular} is added. It will be possible to change in a future version.`"
-            persistent-hint
-            v-model="$v.form.dateProduced.$model"
-            :error-messages="dateProducedErrors"
-          ></v-text-field-with-date-picker>
-        </v-col>
-        <v-col order="4" cols="12" md="10">
-          <v-text-field
-            outlined
-            label="Produced by*"
-            required
-            :hint="`The person or group that produced the ${productType.singular}, e.g. pwg-xxx. In the current version of the product DB, this field cannot be changed once the ${productType.singular} is added. It will be possible to change in a future version.`"
-            persistent-hint
-            v-model="$v.form.producedBy.$model"
-            :error-messages="producedByErrors"
-          ></v-text-field>
-        </v-col>
-        <v-col order="5" cols="12" md="10">
-          <v-text-field
-            outlined
-            label="Contact*"
-            required
-            :hint="`A person or group that can be contacted for questions or issues about the ${productType.singular}.`"
-            persistent-hint
-            v-model="$v.form.contact.$model"
-            :error-messages="contactErrors"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-        <v-col cols="12" md="10">
-          <v-textarea
-            outlined
-            label="Paths"
-            hint="A path per line. e.g., nersc:/go/to/my/product_v3"
-            rows="4"
-            persistent-hint
-            v-model="$v.form.paths.$model"
-          ></v-textarea>
-        </v-col>
-        <v-col cols="12" md="10" class="mt-4">
-          <label class="v-label theme--light">Note</label>
-          <v-tabs v-model="tabNote" class="mb-1">
-            <v-tab key="edit">Edit</v-tab>
-            <v-tab key="preview">Preview</v-tab>
-          </v-tabs>
-          <v-tabs-items v-model="tabNote">
-            <v-tab-item
-              key="edit"
-              style="min-height: 180px"
-              :transition="false"
-              :reverse-transition="false"
-            >
-              <v-textarea
-                solo
-                outlined
-                label="Note will be parsed as Markdown"
-                rows="5"
-                v-model="$v.form.note.$model"
-              ></v-textarea>
-            </v-tab-item>
-            <v-tab-item
-              key="preview"
-              v-html="noteMarked"
-              style="min-height: 180px"
-              :transition="false"
-              :reverse-transition="false"
-              class="markdown-body px-3 pt-3"
-            >
-            </v-tab-item>
-          </v-tabs-items>
-        </v-col>
-      </v-row>
-    </v-container>
+  <v-container fluid>
+    <v-alert v-if="error" type="error">{{ error }}</v-alert>
+    <v-row justify="center">
+      <v-col order="1" cols="12" md="10">
+        <v-text-field
+          outlined
+          label="Name*"
+          required
+          :hint="`Name of the ${productType.singular}.`"
+          persistent-hint
+          :error-messages="nameErrors"
+          :value="$v.form.name.$model"
+          @input="debounceInput($v.form.name, $event)"
+          @blur="$v.form.name.$touch()"
+        ></v-text-field>
+      </v-col>
+      <v-col order="3" cols="12" md="10">
+        <v-text-field-with-date-picker
+          outlined
+          label="Date produced (YYYY-MM-DD)*"
+          required
+          :hint="`The date on which the ${productType.singular} was produced, e.g., 2020-05-06. In the current version of the product DB, this field cannot be changed once the ${productType.singular} is added. It will be possible to change in a future version.`"
+          persistent-hint
+          v-model="$v.form.dateProduced.$model"
+          :error-messages="dateProducedErrors"
+        ></v-text-field-with-date-picker>
+      </v-col>
+      <v-col order="4" cols="12" md="10">
+        <v-text-field
+          outlined
+          label="Produced by*"
+          required
+          :hint="`The person or group that produced the ${productType.singular}, e.g. pwg-xxx. In the current version of the product DB, this field cannot be changed once the ${productType.singular} is added. It will be possible to change in a future version.`"
+          persistent-hint
+          v-model="$v.form.producedBy.$model"
+          :error-messages="producedByErrors"
+        ></v-text-field>
+      </v-col>
+      <v-col order="5" cols="12" md="10">
+        <v-text-field
+          outlined
+          label="Contact*"
+          required
+          :hint="`A person or group that can be contacted for questions or issues about the ${productType.singular}.`"
+          persistent-hint
+          v-model="$v.form.contact.$model"
+          :error-messages="contactErrors"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" md="10">
+        <v-textarea
+          outlined
+          label="Paths"
+          hint="A path per line. e.g., nersc:/go/to/my/product_v3"
+          rows="4"
+          persistent-hint
+          v-model="$v.form.paths.$model"
+        ></v-textarea>
+      </v-col>
+      <v-col cols="12" md="10" class="mt-4">
+        <label class="v-label theme--light">Note</label>
+        <v-tabs v-model="tabNote" class="mb-1">
+          <v-tab key="edit">Edit</v-tab>
+          <v-tab key="preview">Preview</v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tabNote">
+          <v-tab-item
+            key="edit"
+            style="min-height: 180px"
+            :transition="false"
+            :reverse-transition="false"
+          >
+            <v-textarea
+              solo
+              outlined
+              label="Note will be parsed as Markdown"
+              rows="5"
+              v-model="$v.form.note.$model"
+            ></v-textarea>
+          </v-tab-item>
+          <v-tab-item
+            key="preview"
+            v-html="noteMarked"
+            style="min-height: 180px"
+            :transition="false"
+            :reverse-transition="false"
+            class="markdown-body px-3 pt-3"
+          >
+          </v-tab-item>
+        </v-tabs-items>
+      </v-col>
+    </v-row>
     <v-divider></v-divider>
     <v-card-actions>
       <v-spacer></v-spacer>
@@ -116,7 +114,7 @@
         Next
       </v-btn>
     </v-card-actions>
-  </v-form>
+  </v-container>
 </template>
 
 <script>
