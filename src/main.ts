0710acae5311094212e1d843c6c0f344ba0599cd
Tai Sakuma <tai.sakuma@gmail.com>
2022-08-04 09:46:02 -0400

    install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import { createPinia, PiniaVuePlugin } from "pinia";
 import App from "./App.vue";
 import { router, checkAuthForCurrentRoute } from "./router";
 import store from "./store";
@@ -7,14 +8,18 @@ import { apolloProvider } from "./vue-apollo";
 import Vuelidate from "vuelidate";
 // import InstantSearch from "vue-instantsearch";
 
+Vue.use(PiniaVuePlugin);
 Vue.use(Vuelidate);
 // Vue.use(InstantSearch);
 
 Vue.config.productionTip = false;
 
+const pinia = createPinia();
+
 new Vue({
   router,
   vuetify,
+  pinia,
   store,
   apolloProvider,
   created() {
