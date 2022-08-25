0c2398c1591db41b69c57812027a5ad40c3887a7
Tai Sakuma <tai.sakuma@gmail.com>
2022-08-25 19:01:11 -0400

    update SignInRequired

--- src/views/auth/SignInRequired.vue
+++ src/views/auth/SignInRequired.vue
@@ -1,20 +1,30 @@
 <template>
-  <v-container fill-height fluid>
-    <v-row align="center" justify="center">
-      <v-col class="text-h3 text-center">
-        <div>Sign-in required</div>
-        <v-btn class="mt-5" outlined :to="{ name: 'SignIn' }">
-          Go to the sign-in page
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="g-container">
+    <div>
+      <div class="text-h3 pa-3">Sign-in required</div>
+      <sign-in-card :path="$route.query.path">
+        <template v-slot:title> <span></span> </template>
+      </sign-in-card>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import SignInCard from "@/components/auth/SignInCard.vue";
 
 export default Vue.extend({
   name: "SignInRequired",
+  components: {
+    SignInCard,
+  },
 });
 </script>
+
+<style scoped>
+.g-container {
+  display: grid;
+  height: 100%;
+  place-items: center;
+}
+</style>
