2260b5f2baadab3f0c2740901843f2e55d983e10
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-12 09:42:52 -0400

    rewrite GitHubUserTable with composition api and urql

--- src/components/admin/GitHubUserTable.vue
+++ src/components/admin/GitHubUserTable.vue
@@ -35,55 +35,81 @@
 // https://vuetifyjs.com/en/components/data-tables/#crud-actions
 // https://github.com/vuetifyjs/vuetify/blob/master/packages/docs/src/examples/v-data-table/misc-crud.vue
 
-import { defineComponent } from "vue"
-import { mapActions } from "pinia";
+import { defineComponent, ref, watch } from "vue";
 import { useStore } from "@/stores/main";
+import { useQuery, useMutation } from "@urql/vue";
 
 import ALL_GIT_HUB_USERS from "@/graphql/queries/AllGitHubUsers.gql";
 import UPDATE_GITHUB_ORG_MEMBER_LIST from "@/graphql/mutations/UpdateGitHubOrgMemberLists.gql";
 
+interface GitHubUser {
+  login: string;
+  avatarUrl?: string;
+  url?: string;
+}
+
+interface GitHubUserEdge {
+  node: GitHubUser;
+}
+
+interface GitHubUserConnection {
+  totalCount: number;
+  edges: GitHubUserEdge[];
+}
+
 export default defineComponent({
   name: "GitHubUserTable",
-  data: () => ({
-    allGitHubUsers: null,
-    allGitHubUsersHeaders: [
+  setup() {
+    const store = useStore();
+    const alert = ref(false);
+    const error = ref<any>(null);
+
+    const allGitHubUsers = ref<GitHubUserConnection | null>(null);
+    const query = useQuery<{ allGitHubUsers: GitHubUserConnection }>({
+      query: ALL_GIT_HUB_USERS,
+      variables: { orgMember: true },
+    });
+    watch(query.data, (data) => {
+      if (data) {
+        allGitHubUsers.value = data.allGitHubUsers;
+      }
+    });
+
+    const allGitHubUsersHeaders = ref([
       { text: "", value: "node.avatarUrl", align: "start" },
       { text: "User", value: "node.login" },
       { text: "Name", value: "node.name" },
-    ],
-    alert: false,
-    error: null as any,
-  }),
-  apollo: {
-    allGitHubUsers: {
-      query: ALL_GIT_HUB_USERS,
-      variables: { orgMember: true },
-    },
-  },
-  methods: {
-    async updateOrgMemberList() {
+    ]);
+
+    const { executeMutation } = useMutation(UPDATE_GITHUB_ORG_MEMBER_LIST);
+    async function updateOrgMemberList() {
       try {
-        const { data } = await this.$apollo.mutate({
-          mutation: UPDATE_GITHUB_ORG_MEMBER_LIST,
-        });
-        this.$apollo.queries.allGitHubUsers.refetch();
-        this.apolloMutationCalled();
-        this.setSnackbarMessage("Updated");
-      } catch (error) {
-        this.error = error;
+        const { error } = await executeMutation({});
+        query.executeQuery({ requestPolicy: "network-only" });
+        store.apolloMutationCalled();
+        store.setSnackbarMessage("Updated");
+      } catch (e) {
+        error.value = e;
       }
-    },
-    ...mapActions(useStore, ["apolloMutationCalled", "setSnackbarMessage"]),
-  },
-  watch: {
-    error: function (val, oldVal) {
-      this.alert = val ? true : false;
-    },
-    alert: function (val, oldVal) {
+    }
+
+    watch(error, (val, oldVal) => {
+      alert.value = val ? true : false;
+    });
+
+    watch(alert, (val, oldVal) => {
       if (!val) {
-        this.error = null;
+        error.value = null;
       }
-    },
+    });
+
+    return {
+      alert,
+      error,
+      allGitHubUsers,
+      allGitHubUsersHeaders,
+      updateOrgMemberList,
+    };
   },
 });
 </script>
