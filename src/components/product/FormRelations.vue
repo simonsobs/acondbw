6bdecde94d936e20436e38ded07bd219cc24e36a
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-15 16:17:21 -0400

    rewrite FormRelations with urql

--- src/components/product/FormRelations.vue
+++ src/components/product/FormRelations.vue
@@ -21,65 +21,73 @@
     </v-card-text>
     <div v-if="loaded" class="pb-5">
       <div
-        v-for="({ node }, i) in allProductRelationTypes.edges"
+        v-for="(edge, i) in allProductRelationTypes.edges"
         :key="i"
         class="pb-4"
       >
-        <v-divider></v-divider>
-        <v-card-title class="text-h5 capitalize">
-          {{ node.plural }}
-        </v-card-title>
-        <v-card-text class="grey--text">
-          <span>
-            <span class="font-italic capitalize">
-              {{ node.plural }}
+        <template v-if="edge && edge.node">
+          <v-divider></v-divider>
+          <v-card-title class="text-h5 capitalize">
+            {{ edge.node.plural }}
+          </v-card-title>
+          <v-card-text class="grey--text" v-if="edge.node.reverse">
+            <span>
+              <span class="font-italic capitalize">
+                {{ edge.node.plural }}
+              </span>
+              of <span class="font-italic"> {{ name }}. </span>
+              <span class="font-italic"> {{ name }} </span>
+              will be {{ edge.node.reverse.indefArticle }}
+              <span class="font-italic">
+                {{ edge.node.reverse.singular }}.
+              </span>
             </span>
-            of <span class="font-italic"> {{ name }}. </span>
-            <span class="font-italic"> {{ name }} </span>
-            will be {{ node.reverse.indefArticle }}
-            <span class="font-italic"> {{ node.reverse.singular }}. </span>
-          </span>
-        </v-card-text>
-        <v-container>
-          <v-row
-            v-for="(e, i) in form[node.typeId]"
-            :key="i"
-            class="flex-nowrap"
-          >
-            <v-card-text>
-              <v-autocomplete
-                :label="node.singular"
-                :items="productItems"
-                outlined
-                clearable
-                hide-details
-                hide-no-data
-                v-model="e.productId"
-              ></v-autocomplete>
-            </v-card-text>
-            <v-card-actions>
-              <v-tooltip bottom open-delay="800">
-                <template v-slot:activator="{ on }">
-                  <v-btn icon @click="form[node.typeId].splice(i, 1)" v-on="on">
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </template>
-                <span>Delete the field</span>
-              </v-tooltip>
-            </v-card-actions>
-          </v-row>
-        </v-container>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="secondary"
-            outlined
-            text
-            @click="form[node.typeId].push({ productId: null })"
-          >
-            Add a field
-          </v-btn>
-        </v-card-actions>
+          </v-card-text>
+          <v-container>
+            <v-row
+              v-for="(e, i) in form[edge.node.typeId]"
+              :key="i"
+              class="flex-nowrap"
+            >
+              <v-card-text>
+                <v-autocomplete
+                  :label="edge.node.singular"
+                  :items="productItems"
+                  outlined
+                  clearable
+                  hide-details
+                  hide-no-data
+                  v-model="e.productId"
+                ></v-autocomplete>
+              </v-card-text>
+              <v-card-actions>
+                <v-tooltip bottom open-delay="800">
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      icon
+                      @click="form[edge.node.typeId].splice(i, 1)"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Delete the field</span>
+                </v-tooltip>
+              </v-card-actions>
+            </v-row>
+          </v-container>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="secondary"
+              outlined
+              text
+              @click="form[edge.node.typeId].push({ productId: null })"
+            >
+              Add a field
+            </v-btn>
+          </v-card-actions>
+        </template>
       </div>
     </div>
     <v-card-actions>
@@ -94,26 +102,77 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent, ref, computed, watch, PropType } from "vue";
 import State from "@/utils/LoadingState";
 import DevToolLoadingStateOverridingMenu from "@/components/utils/DevToolLoadingStateOverridingMenu.vue";
 
 import QueryForFormRelations from "@/graphql/queries/QueryForFormRelations.gql";
+import { QueryForFormRelationsQuery } from "@/generated/graphql";
 
-export default {
+import { useQuery } from "@urql/vue";
+
+interface Relation {
+  productId: string;
+  typeId: number;
+}
+
+interface Reshaped {
+  [key: number]: { productId: string | null }[];
+}
+
+function reshapeValue(val: Relation[]): Reshaped {
+  if (!val) return {};
+  return val.reduce((a, o) => {
+    if (o.typeId in a) {
+      a[o.typeId].push({ productId: o.productId });
+      return a;
+    } else {
+      return { ...a, [o.typeId]: [{ productId: o.productId }] };
+    }
+  }, {});
+}
+
+export default defineComponent({
   name: "FormRelations",
   components: {
     DevToolLoadingStateOverridingMenu,
   },
-  props: { value: Array, name: String },
+  props: {
+    value: { type: Array as PropType<Relation[]>, require: true },
+    name: { type: String, require: true },
+  },
+  setup(prop) {
+    const query = useQuery<QueryForFormRelationsQuery>({
+      query: QueryForFormRelations,
+    });
+    const allProductRelationTypes = computed(
+      () => query.data.value?.allProductRelationTypes || { edges: [] }
+    );
+    const allProducts = computed(
+      () => query.data.value?.allProducts || { edges: [] }
+    );
+    const init = ref(true);
+    const queryError = ref<string | null>(null);
+    watch(query.data, (data) => {
+      if (data) init.value = false;
+    });
+    watch(query.error, (e) => {
+      init.value = false;
+      queryError.value = e?.message || null;
+    });
+    return {
+      query,
+      allProductRelationTypes,
+      allProducts,
+      init,
+      queryError,
+    };
+  },
   data() {
     return {
-      form: this.reshapeValue(this.value || []),
+      form: reshapeValue(this.value || []),
       formReset: null,
-      allProductRelationTypes: { edges: [] },
-      allProducts: { edges: [] },
-      init: true,
-      queryError: null,
       refreshing: false,
       devtoolState: null,
       State: State,
@@ -123,7 +182,7 @@ export default {
     state() {
       if (this.devtoolState) return this.devtoolState;
       if (this.refreshing) return State.LOADING;
-      if (this.$apollo.loading) return State.LOADING;
+      if (this.query.fetching.value) return State.LOADING;
       if (this.queryError) return State.ERROR;
       if (this.allProductRelationTypes) return State.LOADED;
       if (this.init) return State.INIT;
@@ -138,12 +197,17 @@ export default {
     notFound() {
       return this.state == State.NONE;
     },
-    input() {
-      return Object.entries(this.form).reduce((a, e) => {
-        const typeId = e[0];
-        const l = e[1].filter((x) => x.productId !== null);
-        return [...a, ...l.map((o) => ({ productId: o.productId, typeId }))];
-      }, []);
+    input(): Relation[] {
+      return Object.entries(this.form)
+        .reduce((a, e) => {
+          const typeId = Number(e[0]);
+          const l = e[1].filter((x) => x.productId !== null);
+          return [...a, ...l.map((o) => ({ productId: o.productId, typeId }))];
+        }, [] as Relation[])
+        .sort(
+          (a, b) =>
+            a.typeId - b.typeId || a.productId.localeCompare(b.productId)
+        );
     },
     unchanged() {
       if (!this.formReset) return false;
@@ -175,7 +239,7 @@ export default {
         this.devtoolState == State.ERROR ? "Error from Dev Tools" : null;
     },
     allProductRelationTypes(val) {
-      const reshapedValue = this.reshapeValue(this.value);
+      const reshapedValue = this.value ? reshapeValue(this.value) : {};
       this.form = this.composeForm(val, reshapedValue);
       if (!this.formReset) {
         this.formReset = JSON.parse(JSON.stringify(this.form));
@@ -189,42 +253,17 @@ export default {
       immediate: true,
     },
   },
-  apollo: {
-    allProductRelationTypes: {
-      query: QueryForFormRelations,
-      result(result) {
-        this.init = false;
-
-        this.queryError = result.error || null;
-        if (this.queryError) return;
-
-        this.allProductRelationTypes = result.data.allProductRelationTypes;
-        this.allProducts = result.data.allProducts;
-      },
-    },
-  },
   methods: {
     async refetch() {
       this.refreshing = true;
       const wait = new Promise((resolve) => setTimeout(resolve, 500));
-      Object.values(this.$apollo.queries).forEach((query) => query.refetch());
+      await this.query.executeQuery({ requestPolicy: "network-only" });
       await wait; // wait until 0.5 sec passes since starting refetch
       // because the progress circular is too flickering if
       // the refetch finishes too quickly
       this.refreshing = false;
     },
-    reshapeValue(val) {
-      if (!val) return {};
-      return val.reduce((a, o) => {
-        if (o.typeId in a) {
-          a[o.typeId].push({ productId: o.productId });
-          return a;
-        } else {
-          return { ...a, [o.typeId]: [{ productId: o.productId }] };
-        }
-      }, {});
-    },
-    composeForm(allProductRelationTypes, reshapedValue) {
+    composeForm(allProductRelationTypes, reshapedValue: Reshaped): Reshaped {
       return allProductRelationTypes.edges.reduce(
         (a, { node }) => ({
           ...a,
@@ -240,5 +279,5 @@ export default {
       this.form = JSON.parse(JSON.stringify(this.formReset));
     },
   },
-};
+});
 </script>
