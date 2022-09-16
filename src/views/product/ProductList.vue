3d25c8be73df8eb237b1a868f395dd7f5255013f
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-16 18:53:55 -0400

    format source

--- src/views/product/ProductList.vue
+++ src/views/product/ProductList.vue
@@ -192,7 +192,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 import { mapState } from "pinia";
 import { useStore } from "@/stores/main";
 
