98e7a7ed974dbd6e3519b25625faba9c4cf1c39f
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-23 18:01:02 -0400

    rewrite GitHubOrgRemoveForm with script setup

--- src/components/admin/GitHubOrgRemoveForm.vue
+++ src/components/admin/GitHubOrgRemoveForm.vue
@@ -4,63 +4,64 @@
     <v-card-text>
       <v-alert v-if="error" type="error">{{ error }}</v-alert>
     </v-card-text>
-    <v-card-text class="body-1 font-weight-medium error--text"
-      >Really, remove "{{ login }}" ?</v-card-text
-    >
+    <v-card-text class="body-1 font-weight-medium error--text">
+      Really, remove "{{ login }}" ?
+    </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
       <v-btn color="secondary" text @click="cancel">Cancel</v-btn>
-      <v-btn color="error" text @click="remove">Remove</v-btn>
+      <v-btn color="error" :disabled="!props.login" text @click="remove">
+        Remove
+      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import { mapActions } from "pinia";
+<script setup lang="ts">
+import { ref, defineProps, defineEmits } from "vue";
 import { useStore } from "@/stores/main";
 
-import { client } from "@/plugins/urql";
+import { useDeleteGitHubOrgMutation } from "@/generated/graphql";
 
-import DELETE_GITHUB_ORG from "@/graphql/mutations/DeleteGitHubOrg.gql";
+const props = defineProps<{
+  login?: string;
+}>();
 
-export default defineComponent({
-  name: "GitHubOrgRemoveForm",
-  props: {
-    login: String,
-  },
-  data: () => ({
-    error: null as any,
-  }),
-  methods: {
-    cancel() {
-      this.$emit("cancel");
-      this.delayedReset();
-    },
-    delayedReset() {
-      // reset 0.5 sec after so that the reset form won't be shown.
-      setTimeout(() => {
-        this.reset();
-      }, 500);
-    },
-    reset() {
-      this.error = null;
-    },
-    async remove() {
-      try {
-        const result = await client
-          .mutation(DELETE_GITHUB_ORG, { login: this.login })
-          .toPromise();
-        if (result.error) throw result.error;
-        this.apolloMutationCalled();
-        this.setSnackbarMessage("Removed");
-        this.$emit("finished");
-        this.delayedReset();
-      } catch (error) {
-        this.error = error;
-      }
-    },
-    ...mapActions(useStore, ["apolloMutationCalled", "setSnackbarMessage"]),
-  },
-});
+const emit = defineEmits(["cancel", "finished"]);
+
+const store = useStore();
+const error = ref<any>(null);
+
+function cancel() {
+  emit("cancel");
+  delayedReset();
+}
+
+function delayedReset() {
+  // reset 0.5 sec after so that the reset form won't be shown.
+  setTimeout(() => {
+    reset();
+  }, 500);
+}
+
+function reset() {
+  error.value = null;
+}
+
+const { executeMutation } = useDeleteGitHubOrgMutation();
+async function remove() {
+  try {
+    if(!props.login) throw new Error("login is required");
+    const { error } = await executeMutation({
+      login: props.login,
+    });
+    if (error) throw error;
+    store.apolloMutationCalled();
+    store.setSnackbarMessage("Removed");
+    emit("finished");
+    delayedReset();
+  } catch (e) {
+    error.value = e;
+  }
+}
 </script>
