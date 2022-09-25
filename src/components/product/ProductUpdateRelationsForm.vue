b12cb8f269ed1de760d204d1d3c071d5330ab705
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-24 23:39:03 -0400

    rewrite ProductUpdateRelationsForm with composition api

--- src/components/product/ProductUpdateRelationsForm.vue
+++ src/components/product/ProductUpdateRelationsForm.vue
@@ -1,5 +1,6 @@
 <template>
   <v-card>
+  <pre>{{ relations }}</pre>
     <v-card-title class="primary--text">
       <span>
         Update the relations of
@@ -24,90 +25,100 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
-import { mapActions } from "pinia";
+import { defineComponent, PropType, ref, computed } from "vue";
 import { useStore } from "@/stores/main";
 
 import _ from "lodash";
 
-import UPDATE_PRODUCT from "@/graphql/mutations/UpdateProduct.gql";
 import FormRelations from "./FormRelations.vue";
-import { client } from "@/plugins/urql";
 
-import { Product, ProductRelationEdge } from "@/generated/graphql";
+import { useProductQuery, useUpdateProductMutation } from "@/generated/graphql";
+
+type Product = NonNullable<
+  NonNullable<
+    NonNullable<ReturnType<typeof useProductQuery>["data"]>["value"]
+  >["product"]
+>;
+
+type ProductRelationEdge = NonNullable<
+  NonNullable<Product["relations"]>["edges"][0]
+>;
 
 function composeRelation(edge: ProductRelationEdge) {
   const productId = edge?.node?.other?.productId;
   const typeId = edge?.node?.typeId;
   if (!productId || !typeId) return null; // TODO: throw error
-  return { productId, typeId };
+  return { productId: Number(productId), typeId };
 }
 
 function composeRelations(node: Product) {
   return (
     node.relations?.edges.flatMap((e) => (e ? composeRelation(e) || [] : [])) ||
     []
-  ).sort(
-    (a, b) => a.typeId - b.typeId || a.productId.localeCompare(b.productId)
-  );
+  ).sort((a, b) => a.typeId - b.typeId || a.productId - b.productId);
 }
 
 export default defineComponent({
   name: "ProductUpdateRelationsForm",
-  components: {
-    FormRelations,
-  },
+  components: { FormRelations },
   props: {
     node: {
       type: Object as PropType<Product>,
       required: true,
     },
   },
-  data() {
-    const initialRelations = composeRelations(this.node);
-    return {
-      initialRelations,
-      relations: JSON.parse(
-        JSON.stringify(initialRelations)
-      ) as typeof initialRelations,
-      error: null as any,
-    };
-  },
-  computed: {
-    input() {
-      return this.composeInput(this.relations);
-    },
-    unchanged() {
-      return (
-        JSON.stringify(this.relations) === JSON.stringify(this.initialRelations)
-      );
-    },
-  },
-  methods: {
-    composeInput<T>(relations: T[]): T[] {
+  setup(prop, { emit }) {
+    const error = ref<any>(null);
+    const store = useStore();
+
+    const initialRelations = ref<ReturnType<typeof composeRelations>>(
+      composeRelations(prop.node)
+    );
+    const relations = ref<typeof initialRelations>(
+      JSON.parse(JSON.stringify(initialRelations.value))
+    );
+
+    const input = computed(() => composeInput(relations.value));
+
+    function composeInput<T>(relations: T[]): T[] {
       // unique https://medium.com/coding-at-dawn/how-to-use-set-to-filter-unique-items-in-javascript-es6-196c55ce924b
       return [...new Set(relations.map((o) => JSON.stringify(o)))].map((s) =>
         JSON.parse(s)
       );
-    },
-    async submit() {
+    }
+
+    const unchanged = computed(
+      () =>
+        JSON.stringify(relations.value) ===
+        JSON.stringify(initialRelations.value)
+    );
+
+    const { executeMutation } = useUpdateProductMutation();
+
+    async function submit() {
       try {
-        const updateProductInput = { relations: this.input };
-        const { error, data } = await client
-          .mutation(UPDATE_PRODUCT, {
-            productId: this.node.productId,
-            input: updateProductInput,
-          })
-          .toPromise();
+        const updateProductInput = { relations: input.value };
+        const { error } = await executeMutation({
+          productId: Number(prop.node.productId),
+          input: updateProductInput,
+        });
         if (error) throw error;
-        this.apolloMutationCalled();
-        this.setSnackbarMessage("Updated");
-        this.$emit("finished");
-      } catch (error) {
-        this.error = error;
+        store.apolloMutationCalled();
+        store.setSnackbarMessage("Updated");
+        emit("finished");
+      } catch (e) {
+        error.value = e;
       }
-    },
-    ...mapActions(useStore, ["apolloMutationCalled", "setSnackbarMessage"]),
+    }
+
+    return {
+      error,
+      initialRelations,
+      relations,
+      input,
+      unchanged,
+      submit,
+    };
   },
 });
 </script>
