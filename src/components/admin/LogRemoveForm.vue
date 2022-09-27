7e4e110e8f1906844241bd332bbb449dc9f9de7c
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-27 18:15:44 -0400

    remove unnecessary import

--- src/components/admin/LogRemoveForm.vue
+++ src/components/admin/LogRemoveForm.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, defineEmits } from "vue";
+import { ref } from "vue";
 import { useStore } from "@/stores/main";
 
 import { useDeleteLogMutation } from "@/generated/graphql";
