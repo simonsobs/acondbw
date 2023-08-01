15c2173fa1688f461dc64c0a719dcba7bab0819d
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-01 11:41:54 -0400

    use vuetify labs

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,12 +1,14 @@
-import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css';
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 
+import * as labsComponents from 'vuetify/labs/components';
+
 const vuetify = createVuetify({
-  components,
+  components: { ...components, ...labsComponents },
   directives,
   icons: {
     defaultSet: "mdi",
