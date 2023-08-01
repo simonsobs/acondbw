43e5fa6fe57870348e16136fdcd1e7cbac87ba4c
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-01 13:41:14 -0400

    update Entry.vue

--- src/views/framework/Entry.vue
+++ src/views/framework/Entry.vue
@@ -1,26 +1,12 @@
 <template>
-  <v-container fill-height fluid style="align-items: stretch">
-    <v-row align="center" justify="center">
-      <v-col cols="12" md="6">
-        <div class="text-h3 text-center">
-          {{ title }}
-        </div>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-container fill-height fluid>
-          <v-row align="center" justify="center">
-            <v-col style="flex: 0">
-              <sign-in-card></sign-in-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="g-container">
+    <div class="text-h3 g-title"> {{ title }} </div>
+    <sign-in-card></sign-in-card>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue"
+import { computed } from "vue";
 import { useConfigStore } from "@/stores/config";
 
 import SignInCard from "@/components/auth/SignInCard.vue";
@@ -28,3 +14,21 @@ import SignInCard from "@/components/auth/SignInCard.vue";
 const configStore = useConfigStore();
 const title = computed(() => configStore.config.toolbarTitle || "");
 </script>
+
+<style scoped>
+.g-container {
+  display: grid;
+  block-size: 100%;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 6px;
+  justify-items: center;
+  align-items: center;
+  align-content: space-around;
+}
+
+.g-title {
+  inline-size: fit-content;
+  text-align: center;
+  margin: 8px;
+}
+</style>
