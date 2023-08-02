32d19aa4c26a00c2cedf02777f48c4903fd2d159
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-02 09:24:52 -0400

    update UserMenuButton.vue

--- src/components/auth/UserMenuButton.vue
+++ src/components/auth/UserMenuButton.vue
@@ -2,52 +2,31 @@
   <span>
     <span v-if="user">
       <v-menu bottom offset-y v-model="menu">
-        <template v-slot:activator="{ on, attr }">
-          <v-btn icon class="no-uppercase" v-bind="attr" v-on="on">
-            <v-avatar size="36">
-              <img :src="user.avatarUrl" :alt="user.login" />
+        <template v-slot:activator="{ props }">
+          <v-btn icon class="no-uppercase" v-bind="props">
+            <v-avatar size="36px">
+              <v-img :src="user.avatarUrl" :alt="user.login"></v-img>
             </v-avatar>
           </v-btn>
         </template>
         <v-list>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>{{ user.name }}</v-list-item-title>
-              <v-list-item-subtitle>{{ user.login }}</v-list-item-subtitle>
-            </v-list-item-content>
-            <v-list-item-icon>
-              <v-icon left>mdi-github</v-icon>
-            </v-list-item-icon>
+          <v-list-item :title="user.name" :subtitle="user.login" append-icon="mdi-github">
           </v-list-item>
           <v-divider></v-divider>
           <template v-if="isAdmin">
-            <v-list-item :to="{ name: 'Admin' }">
-              <v-list-item-icon>
-                <v-icon>mdi-cog</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>Admin</v-list-item-title>
-              </v-list-item-content>
+            <v-list-item :to="{ name: 'Admin' }" prepend-icon="mdi-cog" title="Admin">
             </v-list-item>
             <v-divider></v-divider>
           </template>
           <v-dialog v-model="dialog" max-width="600">
-            <template v-slot:activator="{ on }">
-              <v-list-item v-on="on">
-                <v-list-item-icon>
-                  <v-icon>mdi-logout</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title>Sign Out</v-list-item-title>
-                </v-list-item-content>
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" prepend-icon="mdi-logout" title="Sign Out">
               </v-list-item>
             </template>
-            <sign-out-confirmation
-              v-on:finished="
-                dialog = false;
-                menu = false;
-              "
-            ></sign-out-confirmation>
+            <sign-out-confirmation v-on:finished="
+              dialog = false;
+            menu = false;
+            "></sign-out-confirmation>
           </v-dialog>
         </v-list>
       </v-menu>
