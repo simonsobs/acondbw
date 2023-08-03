c1afa80804e057039522268ebd728a6cbfdf7f5b
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-03 09:49:06 -0400

    add eslint-plugin-vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
 
   extends: [
     "plugin:vue/essential",
+    "plugin:vuetify:base",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "plugin:prettier/recommended",
