964ca06cb4be5ea31fb6eae188f310ebc4e9b256
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-03 11:46:11 -0400

    remove commented out code

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -18,30 +18,3 @@ const vuetify = createVuetify({
 });
 
 export default vuetify;
-
-// import Vue from "vue";
-// import Vuetify from "vuetify/lib/framework";
-
-// import colors from "vuetify/lib/util/colors";
-
-// import "material-design-icons-iconfont/dist/material-design-icons.css";
-
-// Vue.use(Vuetify);
-
-// export default new Vuetify({
-//   icons: {
-//     iconfont: "mdi",
-//   },
-//   theme: {
-//     // dark: false,
-//     // options: { customProperties: true },
-//     // themes: {
-//     //   light: {
-//     //     primary: colors.teal.base,
-//     //     "on-primary": colors.shades.white,
-//     //     secondary: "#b0bec5",
-//     //     anchor: "#8c9eff",
-//     //   },
-//     // },
-//   },
-// });
