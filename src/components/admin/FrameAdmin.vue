654b4a0a091592fbe1598ac46e57c3d8f1a8b2af
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-04 15:41:45 -0400

    rewrite FrameAdmin.vue similar to Frame.vue

--- src/components/admin/FrameAdmin.vue
+++ src/components/admin/FrameAdmin.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
     <navigation-admin v-model="drawer"></navigation-admin>
-    <app-bar v-on:drawer="drawer = !drawer"></app-bar>
+    <app-bar @drawer="drawer = !drawer"></app-bar>
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
+import { useDisplay } from "vuetify";
 import AppBar from "@/components/layout/AppBar.vue";
 import NavigationAdmin from "./NavigationAdmin.vue";
 
-export default {
-  name: "FrameAdmin",
-  components: {
-    AppBar,
-    NavigationAdmin,
-  },
-  data: () => ({
-    drawer: true,
-  }),
-};
+const display = useDisplay();
+const drawer = ref<boolean>(!display.mobile.value);
+watchEffect(() => {
+  drawer.value = !display.mobile.value;
+});
 </script>
