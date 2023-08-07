de89831f6e047a4b15dea92acb659ff1feef4634
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-07 13:55:32 -0400

    update User.vue

--- src/views/admin/User.vue
+++ src/views/admin/User.vue
@@ -1,46 +1,23 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <v-card flat>
-          <v-card-title class="text-h4">Users</v-card-title>
-          <v-card class="my-2">
-            <v-card-title>Tokens with org access</v-card-title>
-            <v-card-text>
-              <git-hub-token-table></git-hub-token-table>
-            </v-card-text>
-          </v-card>
-          <v-card class="my-2">
-            <v-card-title>Organizations</v-card-title>
-            <v-card-text>
-              <git-hub-org-table></git-hub-org-table>
-            </v-card-text>
-          </v-card>
-          <v-card class="my-2">
-            <v-card-title>Organization members</v-card-title>
-            <v-card-text>
-              <git-hub-user-table></git-hub-user-table>
-            </v-card-text>
-          </v-card>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="pt-5 px-5 pb-16" style="max-width: 960px; margin: auto">
+    <div class="text-h4 text-primary">Users</div>
+    <div class="mt-6 mb-2">
+      <div class="text-h5 text-primary">Tokens with org access</div>
+      <git-hub-token-table></git-hub-token-table>
+    </div>
+    <div class="mt-16 mb-2">
+      <div class="text-h5 text-primary">Organizations</div>
+      <git-hub-org-table></git-hub-org-table>
+    </div>
+    <div class="mt-16 mb-2">
+      <div class="text-h5 text-primary">Organization members</div>
+      <git-hub-user-table></git-hub-user-table>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue"
+<script setup lang="ts">
 import GitHubTokenTable from "@/components/admin/GitHubTokenTable.vue";
 import GitHubOrgTable from "@/components/admin/GitHubOrgTable.vue";
 import GitHubUserTable from "@/components/admin/GitHubUserTable.vue";
-
-export default defineComponent({
-  name: "User",
-  components: {
-    GitHubTokenTable,
-    GitHubOrgTable,
-    GitHubUserTable,
-  },
-  data: () => ({}),
-});
 </script>
