f75523815f1c5d0c4567258e4d0437011083358a
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-10 14:07:01 -0400

    update the product convert type form

--- src/components/product/ProductConvertTypeForm.vue
+++ src/components/product/ProductConvertTypeForm.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card>
-    <v-card-title class="primary--text">
+    <v-card-title class="text-primary">
       <span>
         Convert
         <span class="font-italic"> {{ node.name }} </span>
@@ -8,7 +8,7 @@
       </span>
     </v-card-title>
     <v-card-text>
-      <v-alert v-if="error" type="error">{{ error }}</v-alert>
+      <v-alert v-if="error" variant="tonal" type="error"> {{ error }} </v-alert>
       <span v-if="node && node.type_">
         Current type:
         <span class="font-italic"> {{ node.type_.singular }} </span>
@@ -16,7 +16,7 @@
     </v-card-text>
     <v-card-text>
       <v-autocomplete
-        outlined
+        variant="outlined"
         label="New type"
         :items="items"
         v-model="newTypeId"
@@ -24,16 +24,23 @@
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="secondary" text @click="$emit('cancel')"> Cancel </v-btn>
-      <v-btn color="primary" :disabled="unchanged" text @click="submit">
+      <v-btn color="secondary" variant="text" @click="$emit('cancel')">
+        Cancel
+      </v-btn>
+      <v-btn
+        color="primary"
+        :disabled="unchanged"
+        variant="text"
+        @click="submit"
+      >
         Submit
       </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType, ref, computed } from "vue";
+<script setup lang="ts">
+import { ref, computed } from "vue";
 import { useStore } from "@/stores/main";
 
 import {
@@ -49,77 +56,66 @@ type Product = NonNullable<
   >["product"]
 >;
 
-export default defineComponent({
-  name: "ProductConvertTypeForm",
-  props: {
-    node: { type: Object as PropType<Product>, required: true },
-  },
-  setup(prop, { emit }) {
-    const error = ref<any>(null);
-    const store = useStore();
-    const typeId = prop.node.type_?.typeId;
-    const newTypeId = ref<number | undefined>(
-      typeId ? Number(typeId) : undefined
-    );
-    const productTypeQuery = useProductTypeQuery({
-      variables: { typeId: newTypeId },
-    });
-    const productType = computed(
-      () => productTypeQuery.data?.value?.productType
-    );
+interface Props {
+  node: Product;
+}
 
-    const allProductTypesQuery = useAllProductTypesQuery();
-    const allProductTypes = computed(
-      () => allProductTypesQuery.data?.value?.allProductTypes
-    );
+interface Emits {
+  (event: "finished", name: string): void;
+  (event: "cancel"): void;
+}
 
-    const items = computed(
-      () =>
-        allProductTypes?.value?.edges.flatMap((e) =>
-          e?.node
-            ? {
-                text: e.node.singular,
-                value: Number(e.node.typeId),
-              }
-            : []
-        ) || []
-    );
+const prop = defineProps<Props>();
+const emit = defineEmits<Emits>();
 
-    const newTypeName = computed(() => productType.value?.name);
-
-    const unchanged = computed(
-      () =>
-        newTypeId.value === Number(prop.node?.type_?.typeId) &&
-        newTypeName.value === prop.node.type_?.name
-    );
-
-    const { executeMutation } = useConvertProductTypeMutation();
-    async function submit() {
-      try {
-        if(!newTypeId.value) throw new Error("typeId is required");
-        const { error } = await executeMutation({
-          productId: Number(prop.node.productId),
-          typeId: newTypeId.value,
-        });
-        if (error) throw error;
-        store.apolloMutationCalled();
-        store.setSnackbarMessage("Converted");
-        emit("finished", newTypeName.value);
-      } catch (e) {
-        error.value = e;
-      }
-    }
-
-    return {
-      error,
-      newTypeId,
-      productType,
-      allProductTypes,
-      items,
-      newTypeName,
-      unchanged,
-      submit
-    };
-  },
+const error = ref<any>(null);
+const store = useStore();
+const typeId = prop.node.type_?.typeId;
+const newTypeId = ref<number | undefined>(typeId ? Number(typeId) : undefined);
+const productTypeQuery = useProductTypeQuery({
+  variables: { typeId: newTypeId },
 });
+const productType = computed(() => productTypeQuery.data?.value?.productType);
+
+const allProductTypesQuery = useAllProductTypesQuery();
+const allProductTypes = computed(
+  () => allProductTypesQuery.data?.value?.allProductTypes
+);
+
+const items = computed(
+  () =>
+    allProductTypes?.value?.edges.flatMap((e) =>
+      e?.node
+        ? {
+            title: e.node.singular,
+            value: Number(e.node.typeId),
+          }
+        : []
+    ) || []
+);
+
+const newTypeName = computed(() => productType.value?.name);
+
+const unchanged = computed(
+  () =>
+    newTypeId.value === Number(prop.node?.type_?.typeId) &&
+    newTypeName.value === prop.node.type_?.name
+);
+
+const { executeMutation } = useConvertProductTypeMutation();
+async function submit() {
+  try {
+    if (!newTypeId.value) throw new Error("typeId is required");
+    const { error } = await executeMutation({
+      productId: Number(prop.node.productId),
+      typeId: newTypeId.value,
+    });
+    if (error) throw error;
+    store.apolloMutationCalled();
+    store.setSnackbarMessage("Converted");
+    emit("finished", newTypeName.value ?? "");
+  } catch (e) {
+    error.value = e;
+  }
+}
 </script>
