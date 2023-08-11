913984e7fc9439793a0dfaa7e99dd4df60c9a0c6
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-10 20:21:50 -0400

    rewrite AccessDenied.vue with script setup

--- src/views/framework/AccessDenied.vue
+++ src/views/framework/AccessDenied.vue
@@ -1,18 +1,22 @@
 <template>
-  <v-container fill-height fluid>
-    <v-row align="center" justify="center">
-      <v-col class="text-h3 text-center">
-        <div>Access Denied</div>
-        <v-btn class="mt-5" outlined to="/"> Go to the entry page </v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="g-container">
+    <v-col class="text-h3 text-center">
+      <div>Access Denied</div>
+      <v-btn class="mt-5" variant="outlined" :to="to">
+        Go to the entry page
+      </v-btn>
+    </v-col>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue"
-
-export default defineComponent({
-  name: "AccessDenied",
-});
+<script setup lang="ts">
+const to = { name: "Entry" };
 </script>
+
+<style scoped>
+.g-container {
+  display: grid;
+  height: 100%;
+  place-items: center;
+}
+</style>
