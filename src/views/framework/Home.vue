3bb38d0c3d7511b23f38899d99a525c565d7524b
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-10 20:22:33 -0400

    remove script from Home.vue
    It has script setup

--- src/views/framework/Home.vue
+++ src/views/framework/Home.vue
@@ -4,11 +4,6 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-export default defineComponent({ name: "Home" });
-</script>
-
 <script setup lang="ts">
 import Dashboard from "@/components/product/Dashboard.vue";
 </script>
