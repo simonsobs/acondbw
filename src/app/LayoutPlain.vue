b88ac427c23e48fab18ac0d5ab0d36330451689b
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-12 14:18:29 -0400

    add LayoutPlain.vue

---
+++ src/app/LayoutPlain.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-main>
+    <router-view v-slot="{ Component, route }">
+      <keep-alive>
+        <component :key="route.path" :is="Component" />
+      </keep-alive>
+    </router-view>
+  </v-main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.v-main {
+  height: 100%;
+  overflow-y: scroll;
+}
+
+/* v-main__wrap probably doesn't exit in Vuetify 3 */
+.v-main__wrap {
+  height: 100%;
+}
+</style>
