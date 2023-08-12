fa63003da4c93ed449a1a98b3bdb2fa0ba60efd7
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-12 13:26:29 -0400

    make FrameAdmin.vue similar to Frame.vue

--- src/components/admin/FrameAdmin.vue
+++ src/components/admin/FrameAdmin.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
     <navigation-admin v-model="drawer"></navigation-admin>
-    <app-bar @drawer="drawer = !drawer"></app-bar>
+    <app-bar :order="order">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon @click="toggleDrawer" v-if="mobile">
+        </v-app-bar-nav-icon>
+      </template>
+    </app-bar>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
+import { ref, computed, watchEffect } from "vue";
 import { useDisplay } from "vuetify";
 import AppBar from "@/components/layout/AppBar.vue";
 import NavigationAdmin from "./NavigationAdmin.vue";
 
-const display = useDisplay();
-const drawer = ref<boolean>(!display.mobile.value);
+// https://vuetifyjs.com/en/features/display-and-platform/
+const { mobile } = useDisplay();
+
+const drawer = ref<boolean>(false);
 watchEffect(() => {
-  drawer.value = !display.mobile.value;
+  drawer.value = !mobile.value;
 });
+
+function toggleDrawer() {
+  drawer.value = !drawer.value;
+}
+
+// https://vuetifyjs.com/en/features/application-layout/#dynamic-layouts-and-order
+const order = computed(() => (mobile.value ? 0 : -1));
 </script>
