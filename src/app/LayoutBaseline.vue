792a6c81083a8155548d19e9114e606324a4f80d
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-13 08:20:32 -0400

    merge Frame.vue into LayoutBaseline.vue

--- src/app/LayoutBaseline.vue
+++ src/app/LayoutBaseline.vue
@@ -1,5 +1,13 @@
 <template>
-  <frame></frame>
+  <router-view name="navigationDrawer" v-model="drawer"></router-view>
+  <router-view name="appBar" v-slot="{ Component }">
+    <component :is="Component" :order="order">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon @click="toggleDrawer" v-if="mobile">
+        </v-app-bar-nav-icon>
+      </template>
+    </component>
+  </router-view>
   <v-main>
     <router-view v-slot="{ Component, route }">
       <keep-alive>
@@ -10,7 +18,23 @@
 </template>
 
 <script setup lang="ts">
-import Frame from "@/components/layout/Frame.vue";
+import { ref, computed, watchEffect } from "vue";
+import { useDisplay } from "vuetify";
+
+// https://vuetifyjs.com/en/features/display-and-platform/
+const { mobile } = useDisplay();
+
+const drawer = ref<boolean>(false);
+watchEffect(() => {
+  drawer.value = !mobile.value;
+});
+
+function toggleDrawer() {
+  drawer.value = !drawer.value;
+}
+
+// https://vuetifyjs.com/en/features/application-layout/#dynamic-layouts-and-order
+const order = computed(() => (mobile.value ? 0 : -1));
 </script>
 
 <style>
