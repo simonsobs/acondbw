bff06c47a809842c2f06b2c41345ab557350e9cb
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-13 11:36:30 -0400

    add style for markdown

--- src/app/App.vue
+++ src/app/App.vue
@@ -66,6 +66,15 @@ const order = computed(() => (mobile.value ? 0 : -1));
   height: 100%;
 }
 
+/* https://github.com/sindresorhus/github-markdown-css */
+#app .markdown-body {
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 12px;
+}
+
 #app .markdown-body pre code {
   background-color: inherit;
 }
