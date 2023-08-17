fe4fec4f4caa5c042acaa4cb68a9b197c18de832
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-17 15:19:14 -0400

    extract color-theme.ts from VuetifyApp.vue

--- src/app/VuetifyApp.vue
+++ src/app/VuetifyApp.vue
@@ -5,38 +5,9 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onBeforeMount } from "vue";
-import { useTheme } from "vuetify";
-
-import { useConfigStore } from "@/stores/config";
-import { generateLightAndDarkThemesFromSourceColor } from "@/utils/material-color";
-import { useDarkMode } from "@/utils/dark-mode";
-
+import { useColorTheme } from "@/utils/color-theme";
 import App from "./App.vue";
-
-useDarkMode();
-
-const configStore = useConfigStore();
-
-const sourceColor = computed(
-  () => configStore.config.materialDynamicColorSource
-);
-
-const theme = useTheme();
-
-onBeforeMount(() => {
-  setVuetifyTheme();
-});
-
-function setVuetifyTheme() {
-  const [dynamicLight, dynamicDark] = generateLightAndDarkThemesFromSourceColor(
-    sourceColor.value
-  );
-  // @ts-ignore
-  theme.themes.value.light.colors = dynamicLight.colors;
-  // @ts-ignore
-  theme.themes.value.dark.colors = dynamicDark.colors;
-}
+useColorTheme();
 </script>
 
 <style>
---
+++ src/utils/color-theme.ts
@@ -0,0 +1,25 @@
+import { watch } from "vue";
+import { storeToRefs } from "pinia";
+import { useTheme } from "vuetify";
+
+import { useConfigStore } from "@/stores/config";
+import { generateLightAndDarkThemesFromSourceColor } from "@/utils/material-color";
+import { useDarkMode } from "@/utils/dark-mode";
+
+export function useColorTheme() {
+  useDarkMode();
+  const theme = useTheme();
+  const { config } = storeToRefs(useConfigStore());
+  watch(
+    () => config.value.materialDynamicColorSource,
+    (val) => {
+      const [dynamicLight, dynamicDark] =
+        generateLightAndDarkThemesFromSourceColor(val);
+      // @ts-ignore
+      theme.themes.value.light.colors = dynamicLight.colors;
+      // @ts-ignore
+      theme.themes.value.dark.colors = dynamicDark.colors;
+    },
+    { immediate: true }
+  );
+}
