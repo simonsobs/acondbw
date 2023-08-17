5204e1e994a13609c4a3dc523b2f339ae39e952f
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-17 16:21:39 -0400

    rewrite pre-config.ts with useAxios()

--- src/stores/pre-config.ts
+++ src/stores/pre-config.ts
@@ -1,6 +1,6 @@
-import { ref, watchEffect } from "vue";
+import { ref } from "vue";
 import { defineStore } from "pinia";
-import axios from "axios";
+import { useAxios } from "@vueuse/integrations/useAxios";
 import * as path from "path";
 
 interface PreConfig {
@@ -9,21 +9,12 @@ interface PreConfig {
 
 export const usePreConfigStore = defineStore("preConfig", () => {
   const url = ref(path.join(import.meta.env.VITE_PUBLIC_PATH, "config.json"));
-  const loading = ref(true);
-  const error = ref<any>(null);
-  const preConfig = ref<PreConfig>();
 
-  watchEffect(async () => {
-    error.value = null;
-    try {
-      const response = await axios.get<PreConfig>(url.value);
-      preConfig.value = response.data;
-    } catch (e) {
-      error.value = e;
-    } finally {
-      loading.value = false;
-    }
-  });
+  const {
+    data: preConfig,
+    error,
+    isLoading: loading,
+  } = useAxios<PreConfig>(url.value);
 
   return {
     url,
