ce0dc28448e5bd37a576fbc1fdb266972ba8434d
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-18 10:15:21 -0400

    rewrite useAuthStore() with setup function

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,12 +1,14 @@
+import { ref } from "vue";
 import { defineStore } from "pinia";
 import { Client } from "@urql/vue";
 
 import {
   SignInInfo,
-  signIn,
-  signOut,
+  signIn as _signIn,
+  signOut as _signOut,
   restoreFromLocalStorage,
-  isSignedIn,
+  isSignedIn as _isSignedIn,
+  GitHubViewer,
 } from "@/utils/auth";
 
 // https://www.oauth.com/oauth2-servers/access-tokens/access-token-response/
@@ -16,82 +18,92 @@ interface OAuthError {
   error_uri: string;
 }
 
-interface AuthStoreState extends SignInInfo {
-  token: string | null;
-  lastError: OAuthError | null;
-}
+export const useAuthStore = defineStore("auth", () => {
+  const gitHubViewer = ref<GitHubViewer | null>(null);
+  const isSignedIn = ref<boolean>(false);
+  const isAdmin = ref<boolean>(false);
+  const token = ref<string | null>(null);
+  const lastError = ref<OAuthError | null>(null);
 
-export const useAuthStore = defineStore("auth", {
-  state: (): AuthStoreState => {
-    const initialState: AuthStoreState = {
-      token: null,
-      gitHubViewer: null,
-      isSignedIn: false,
-      isAdmin: false,
-      lastError: null,
-    };
+  try {
+    const { token: token_, signInInfo } = restoreFromLocalStorage();
+    setSignInInfo(signInInfo);
+    token.value = token_;
+  } catch {
+    resetSignInInfo();
+    token.value = null;
+  }
+
+  function setSignInInfo(info: SignInInfo) {
+    gitHubViewer.value = info.gitHubViewer;
+    isSignedIn.value = info.isSignedIn;
+    isAdmin.value = info.isAdmin;
+  }
+
+  function resetSignInInfo() {
+    gitHubViewer.value = null;
+    isSignedIn.value = false;
+    isAdmin.value = false;
+  }
+
+  async function checkIfSignedIn(urqlClient: Client) {
     try {
-      const { token, signInInfo } = restoreFromLocalStorage();
-      return {
-        ...initialState,
-        token,
-        ...signInInfo,
-      };
-    } catch (error) {
-      return initialState;
-    }
-  },
-  actions: {
-    setSignInInfo(info) {
-      this.gitHubViewer = info.gitHubViewer;
-      this.isSignedIn = info.isSignedIn;
-      this.isAdmin = info.isAdmin;
-    },
-    resetSignInInfo() {
-      this.gitHubViewer = null;
-      this.isSignedIn = false;
-      this.isAdmin = false;
-    },
-    async checkIfSignedIn(urqlClient: Client) {
-      try {
-        const result = await isSignedIn(urqlClient);
-        if (result) {
-          this.setSignInInfo(result);
-          return;
-        }
-      } catch (error) {}
-      this.signOut();
-    },
-    async signOut() {
-      this.resetSignInInfo();
-      this.token = null;
-      await signOut();
-    },
-    async signIn(
-      code: string,
-      state: string,
-      urqlClient: Client
-    ) {
-      this.lastError = null;
-      try {
-        const { token, signInInfo } = await signIn(code, state, urqlClient);
-        this.token = token;
-        this.setSignInInfo(signInInfo);
-      } catch (error) {
-        this.signOut();
-        throw error;
+      const result = await _isSignedIn(urqlClient);
+      if (result) {
+        setSignInInfo(result);
+        return;
       }
-    },
-    setRequestAuthError(query) {
-      const error: OAuthError = (({ error, error_description, error_uri }) => ({
-        error,
-        error_description,
-        error_uri,
-      }))(query);
-      this.lastError = error;
-    },
-    clearAuthError() {
-      this.lastError = null;
-    },
-  },
+    } catch {}
+    await signOut();
+  }
+
+  async function signOut() {
+    resetSignInInfo();
+    token.value = null;
+    await _signOut();
+  }
+
+  async function signIn(code: string, state: string, urqlClient: Client) {
+    lastError.value = null;
+    try {
+      const { token: token_, signInInfo } = await _signIn(
+        code,
+        state,
+        urqlClient
+      );
+      token.value = token_;
+      setSignInInfo(signInInfo);
+    } catch (error) {
+      signOut();
+      throw error;
+    }
+  }
+
+  function setRequestAuthError(query: any) {
+    const error: OAuthError = (({ error, error_description, error_uri }) => ({
+      error,
+      error_description,
+      error_uri,
+    }))(query);
+    lastError.value = error;
+  }
+
+  function clearAuthError() {
+    lastError.value = null;
+  }
+
+  return {
+    gitHubViewer,
+    isSignedIn,
+    isAdmin,
+    token,
+    lastError,
+    setSignInInfo,
+    resetSignInInfo,
+    checkIfSignedIn,
+    signOut,
+    signIn,
+    setRequestAuthError,
+    clearAuthError,
+  };
 });
