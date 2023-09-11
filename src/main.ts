4d2277ebb30d4f7b078568fc32454884b13e896a
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-11 17:58:33 -0400

    clean code

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { createApp, h } from "vue";
 import pinia from "@/plugins/pinia";
 import router from "@/plugins/router";
 import vuetify from "@/plugins/vuetify";
+
 import App from "@/app/App.vue";
+
 import DevToolLoadingStateMenu from "@/components/utils/DevToolLoadingStateMenu.vue";
 
-const app = createApp({
-  render: () => h(App),
-});
-app.component("DevToolLoadingStateMenu", DevToolLoadingStateMenu);
-app.use(pinia);
-app.use(router);
-app.use(vuetify);
-app.mount("#app");
+createApp({ render: () => h(App) })
+  .component("DevToolLoadingStateMenu", DevToolLoadingStateMenu)
+  .use(pinia)
+  .use(router)
+  .use(vuetify)
+  .mount("#app");
