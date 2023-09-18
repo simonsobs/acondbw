40f0c9a2717d2548e109459ac81020167e4557ad
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-18 19:32:55 -0400

    move ProductAdd.vue to add/ProductAdd.vue

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -21,7 +21,7 @@ const SignInError = () => import("@/views/auth/SignInError.vue");
 const ProductTop = () => import("@/views/product/top/ProductTop.vue");
 const ProductList = () => import("@/views/product/list/ProductList.vue");
 const ProductItem = () => import("@/views/product/item/ProductItem.vue");
-const ProductAdd = () => import("@/views/product/ProductAdd.vue");
+const ProductAdd = () => import("@/views/product/add/ProductAdd.vue");
 
 const ProductType = () => import("@/views/admin/ProductType.vue");
 
