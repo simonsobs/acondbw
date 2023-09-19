59d9919a05871b379b772c52de1431887a38dbe6
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-19 19:01:43 -0400

    replace echo with set -x

--- docker/docker-entrypoint.d/93-update-config.sh
+++ docker/docker-entrypoint.d/93-update-config.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 # Update config.json
 
-source $(dirname -- "$0")/envvar.sh;
+source $(dirname -- "$0")/envvar.sh
 
 CONFIG_JSON="config.json"
 
-if [[ ! -d ${SITE_DIR} ]]; then
-    echo "Error: ${SITE_DIR} doesn't exit!"
+[ -d "${SITE_DIR}" ] || {
+    echo "Error: ${SITE_DIR} doesn't exist!"
     exit 1
-fi
+}
 
-if [[ ! -f ${SITE_DIR}/${CONFIG_JSON} ]]; then
-    echo "Error: ${SITE_DIR}/${CONFIG_JSON} doesn't exit!"
+[ -f "${SITE_DIR}/${CONFIG_JSON}" ] || {
+    echo "Error: ${SITE_DIR}/${CONFIG_JSON} doesn't exist!"
     exit 1
-fi
+}
 
 (
-    command="cd ${SITE_DIR}"
-    echo + $command
-    $command
-    command="echo \$(jq --arg graphql_http \"\${GRAPHQL_HTTP}\" '.graphqlHttp = (\$graphql_http)' \${CONFIG_JSON}) > \${CONFIG_JSON}"
-    echo + $command
-    eval $command
+    set -x
+    cd $SITE_DIR
+    echo $(jq --arg graphql_http "${GRAPHQL_HTTP}" '.graphqlHttp = ($graphql_http)' ${CONFIG_JSON}) >${CONFIG_JSON}
 )
