f117ef37f288af312dbae7165586dc34f24a4ad8
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-19 20:47:54 -0400

    write all env vars to config file

--- docker/docker-entrypoint.d/93-update-config.sh
+++ docker/docker-entrypoint.d/93-update-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Update config.json
+# Update config.json with all env variables
 
 source $(dirname -- "$0")/envvar.sh
 
@@ -15,8 +15,36 @@ CONFIG_JSON="config.json"
     exit 1
 }
 
+function toCamelCase() {
+    # e.g., GRAPHQL_HTTP -> graphqlHttp
+
+    # Split the string into an array by underscore
+    IFS="_" read -ra words <<<"$1"
+    
+    # Convert the first word to all lower case
+    # Capitalize the first letter of each of subsequent words
+    for i in "${!words[@]}"; do
+        words[$i]=$(echo "${words[$i]}" | tr '[:upper:]' '[:lower:]')
+        if [ $i -gt 0 ]; then
+            words[$i]=$(echo "${words[$i]}" | sed -r 's/(^|_)([a-z])/\U\2/g')
+        fi
+    done
+
+    # Join the array elements with no delimiter
+    con=${words[@]}
+    echo ${con// /}
+}
+
 (
     set -x
     cd $SITE_DIR
-    echo $(jq --arg graphql_http "${GRAPHQL_HTTP}" '.graphqlHttp = ($graphql_http)' ${CONFIG_JSON}) >${CONFIG_JSON}
+    
+    # Loop over all env variables
+    for var in $(compgen -e); do
+        var_camel_case=$(toCamelCase $var)
+        echo $(jq ".${var_camel_case} |= \"${!var}\"" ${CONFIG_JSON}) >${CONFIG_JSON}
+    done
+
+    # Prettify the json file
+    jq . ${CONFIG_JSON} >${CONFIG_JSON}.tmp && mv ${CONFIG_JSON}.tmp ${CONFIG_JSON}
 )
