8618302eb7725573b14d906aa515d2161d2dc75a
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-21 15:49:31 -0400

    open external links in notes in new tab

--- src/components/product/item-card/note/__tests__/parse.spec.ts
+++ src/components/product/item-card/note/__tests__/parse.spec.ts
@@ -6,4 +6,25 @@ describe("useParsed()", () => {
     const { parsed } = useParsed("# Hello");
     expect(parsed.value).toBe("<h1>Hello</h1>\n");
   });
+
+  it("should open external links in new tab and add an icon", () => {
+    // const { parsed } = useParsed("[link](https://example.com)");
+    const { parsed } = useParsed(
+      "- [link1](https://example.com)\n- [link2](https://example.com)"
+    );
+    const expected =
+      "<ul>\n" +
+      "<li>" +
+      '<a href="https://example.com" target="_blank" rel="noopener noreferrer">' +
+      "link1" +
+      ' <i class="mdi mdi-open-in-new mdi v-icon notranslate v-icon--size-xsmall" aria-hidden="true"></i>' +
+      "</a>" +
+      "</li>\n" +
+      '<li><a href="https://example.com" target="_blank" rel="noopener noreferrer">' +
+      "link2" +
+      ' <i class="mdi mdi-open-in-new mdi v-icon notranslate v-icon--size-xsmall" aria-hidden="true"></i>' +
+      "</a></li>\n" +
+      "</ul>\n";
+    expect(parsed.value).toBe(expected);
+  });
 });
