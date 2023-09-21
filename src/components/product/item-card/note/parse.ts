042c026274f3dc02553b3ca4af87029ba6ae6645
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-21 09:55:05 -0400

    use highlight.js

--- src/components/product/item-card/note/parse.ts
+++ src/components/product/item-card/note/parse.ts
@@ -1,8 +1,20 @@
 import { computed, toValue } from "vue";
 import type { MaybeRefOrGetter } from "vue";
-import { marked } from "marked";
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from "highlight.js";
+
+const marked = new Marked(
+  markedHighlight({
+    langPrefix: "hljs language-",
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, { language }).value;
+    },
+  })
+);
 
 export function useParsed(markdown: MaybeRefOrGetter<string>) {
-  const parsed = computed(() => marked(toValue(markdown)));
+  const parsed = computed(() => marked.parse(toValue(markdown)));
   return { parsed };
 }
