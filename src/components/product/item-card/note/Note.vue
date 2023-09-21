aa090cfcdacca75ac634dc9be34c7714d16c6e9c
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-21 09:57:39 -0400

    remove prismjs

--- src/components/product/item-card/note/Note.vue
+++ src/components/product/item-card/note/Note.vue
@@ -5,7 +5,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useParsed } from "./parse";
-import { usePrism } from "./prism";
 interface Props {
   markdown: string;
 }
@@ -13,5 +12,4 @@ const prop = defineProps<Props>();
 const div = ref<HTMLDivElement>();
 const markdown = () => prop.markdown;
 const { parsed: html } = useParsed(markdown);
-usePrism(div);
 </script>
