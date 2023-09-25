1d70ea04de7ec390f817b13175ab399f37e8ad3e
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-25 13:15:23 -0400

    move useEditHtml() to a new file

--- src/components/product/item-card/note/parse.ts
+++ src/components/product/item-card/note/parse.ts
@@ -1,35 +1,10 @@
-import { computed, unref } from "vue";
 import type { MaybeRef } from "vue";
-import $ from "cash-dom";
 
 import { useMarkdown } from "@/utils/markdown";
+import { useEditHtml } from "./edit-html";
 
 export function useParsed(markdown: MaybeRef<string>) {
-  const { html: _parsed } = useMarkdown(unref(markdown));
-  const parsed = useEditHtml(_parsed);
+  const { html } = useMarkdown(markdown);
+  const parsed = useEditHtml(html);
   return { parsed };
-}
-
-function useEditHtml(html: MaybeRef<string>) {
-  const edited = computed(() => edit(html));
-  return edited;
-}
-
-function edit(parsed: MaybeRef<string>) {
-  const tree = $(`<div>${unref(parsed)}</div>`);
-
-  // Open external links in new tab
-  tree
-    .find("a[href^='http']")
-    .attr("target", "_blank")
-    .attr("rel", "noopener noreferrer");
-
-  // Add mdi-open-in-new icon to links with target="_blank" on text (not img)
-  tree
-    .find("a[target='_blank']")
-    .append(
-      ' <i class="mdi mdi-open-in-new mdi v-icon notranslate v-icon--size-xsmall" aria-hidden="true"></i>'
-    );
-
-  return tree.html();
 }
