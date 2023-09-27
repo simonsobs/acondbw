faf6118e9f0bece29260a51adc5f8ca0d0a59056
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-27 10:03:22 -0400

    extract useConnection from useQuery

--- src/components/product/dashboard/query.ts
+++ src/components/product/dashboard/query.ts
@@ -1,20 +1,17 @@
 import { computed, ref } from "vue";
+import type { Ref } from "vue";
 import { useAllProductTypesQuery } from "@/graphql/codegen/generated";
 
 export function useQuery() {
   const query = useAllProductTypesQuery();
   const connection = computed(() => query.data?.value?.allProductTypes);
-  const notFound = computed(() => connection.value === null);
-  const edges = computed(
-    () => connection.value?.edges.flatMap((e) => (e ? [e] : [])) || []
-  );
-  const nodes = computed(() => edges.value.flatMap((e) => e.node || []));
-  const empty = computed(() => nodes.value.length === 0);
 
   const loading = ref(query.fetching);
   const error = ref(query.error);
   const refresh = async () =>
     await query.executeQuery({ requestPolicy: "network-only" });
+
+  const { notFound, edges, nodes, empty } = useConnection(connection);
 
   return {
     query,
@@ -27,4 +24,23 @@ export function useQuery() {
     error,
     refresh,
   };
+}
+
+interface Edge<T> {
+  node?: T | null | undefined;
+}
+
+interface Connection<T> {
+  edges: (Edge<T> | null)[];
+}
+
+function useConnection<T>(connection: Ref<Connection<T> | null | undefined>) {
+  const notFound = computed(() => connection.value === null);
+  const edges = computed(
+    () => connection.value?.edges.flatMap((e) => (e ? [e] : [])) || []
+  );
+  const nodes = computed(() => edges.value.flatMap((e) => e.node || []));
+  const empty = computed(() => nodes.value.length === 0);
+
+  return { notFound, edges, nodes, empty };
 }
